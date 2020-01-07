749c433fc5d666f02033c25a4270c5c1850b4c04
hoojo <hoojo@qq.com>
2020-01-07 18:02:56 +0800

    :sparkles: :bento: :recycle:  Changed 静态导入

--- src/main/java/io/github/hooj0/generic/staticimport/StaticImport.java
+++ src/main/java/io/github/hooj0/generic/staticimport/StaticImport.java
@@ -4,14 +4,15 @@ package io.github.hooj0.generic.staticimport;
 //静态导入TestUtils,.*是所有
 import static io.github.hooj0.generic.source.TestUtils.*;
 
+/**
+ * 静态导入
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date Oct 16, 2010 3:07:56 PM
+ */
 public class StaticImport {
 
-	/**
-	 * 
-	 * @author hoojo
-	 * @createDate Oct 16, 2010 3:07:56 PM
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		System.out.println(NAME = "bbz");
 		System.out.println(getName());
