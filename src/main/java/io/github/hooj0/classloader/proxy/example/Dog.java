63cadfc6409a54122a7c6321712bf1af375556bd
hoojo <hoojo@qq.com>
2020-01-12 12:16:56 +0800

    :sparkles: :bento: :recycle:  Changed 狗

--- src/main/java/io/github/hooj0/classloader/proxy/example/Dog.java
+++ src/main/java/io/github/hooj0/classloader/proxy/example/Dog.java
@@ -1,6 +1,14 @@
 package io.github.hooj0.classloader.proxy.example;
 
+/**
+ * 狗
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/02/08 09:50:24
+ */
 public interface Dog {
-	public void info();
-	public void run();
+    public void info();
+
+    public void run();
 }
