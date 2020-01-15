239ce9192d5dc7573a27667b31d80e9c51572ed3
hoojo <hoojo@qq.com>
2020-01-15 16:09:56 +0800

    :sparkles: :bento: :recycle:  Changed 实现类不会继承接口的注解

--- src/main/java/io/github/hooj0/annotation/inher/ChildImpl.java
+++ src/main/java/io/github/hooj0/annotation/inher/ChildImpl.java
@@ -1,7 +1,11 @@
 package io.github.hooj0.annotation.inher;
 
-/*
+/**
  * 实现类不会继承接口的注解
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/02/08 10:01:14
  */
 @SuppressWarnings("ALL")
 public class ChildImpl implements ParentInterface {
