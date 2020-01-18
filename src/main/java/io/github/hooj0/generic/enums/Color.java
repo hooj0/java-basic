38f130af9cfa252461afbb9de41d4f84cb11f952
hoojo <hoojo@qq.com>
2020-01-18 17:33:56 +0800

    :sparkles: :bento: :recycle:  Changed 颜色

--- src/main/java/io/github/hooj0/generic/enums/Color.java
+++ src/main/java/io/github/hooj0/generic/enums/Color.java
@@ -1,5 +1,12 @@
 package io.github.hooj0.generic.enums;
 
+/**
+ * 颜色
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/02/07 17:18:18
+ */
 public enum Color {
 	RED, WHITE, BLUE, YELLOW;//在枚举中，多个属性接受后。如果还有内容就需要用；号分隔
 	
