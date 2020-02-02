f5d5b102223458a7011446f6c6bfbc0f0e13fda2
hoojo <hoojo@qq.com>
2020-02-02 09:44:56 +0800

    :sparkles: :bento: :recycle:  Changed 链接哈希映射表测试

--- src/main/java/io/github/hooj0/collection/map/LinkHashMapTest.java
+++ src/main/java/io/github/hooj0/collection/map/LinkHashMapTest.java
@@ -2,14 +2,15 @@ package io.github.hooj0.collection.map;
 
 import java.util.LinkedHashMap;
 
+/**
+ * 链接哈希映射表测试
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date Jan 12, 2011 10:40:00 PM
+ */
 public class LinkHashMapTest {
 
-	/**
-	 * 
-	 * @author hoojo
-	 * @createDate Jan 12, 2011 10:40:00 PM
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		LinkedHashMap<String, Integer> scores = new LinkedHashMap<String, Integer>();
 		scores.put("语文", 80);
