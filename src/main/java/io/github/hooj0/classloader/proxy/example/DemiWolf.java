e955a4db58940944e852f184fe2afd76126afb73
hoojo <hoojo@qq.com>
2020-02-02 20:33:56 +0800

    :sparkles: :bento: :recycle:  Changed 黛米·沃尔夫

--- src/main/java/io/github/hooj0/classloader/proxy/example/DemiWolf.java
+++ src/main/java/io/github/hooj0/classloader/proxy/example/DemiWolf.java
@@ -1,12 +1,21 @@
 package io.github.hooj0.classloader.proxy.example;
 
+/**
+ * 黛米·沃尔夫
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/02/08 09:50:11
+ */
 public class DemiWolf implements Dog {
 
-	public void info() {
-		System.out.println("I‘m is a dog！");
-	}
+    @Override
+    public void info() {
+        System.out.println("I‘m is a dog！");
+    }
 
-	public void run() {
-		System.out.println("I'm is run！！");
-	}
+    @Override
+    public void run() {
+        System.out.println("I'm is run！！");
+    }
 }
