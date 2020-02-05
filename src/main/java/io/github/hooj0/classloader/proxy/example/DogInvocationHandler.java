c5e8a89e0d37f913e388e0100391f9af9a259904
hoojo <hoojo@qq.com>
2020-02-05 21:10:56 +0800

    :sparkles: :bento: :recycle:  Changed 狗调用处理程序

--- src/main/java/io/github/hooj0/classloader/proxy/example/DogInvocationHandler.java
+++ src/main/java/io/github/hooj0/classloader/proxy/example/DogInvocationHandler.java
@@ -3,20 +3,30 @@ package io.github.hooj0.classloader.proxy.example;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
+/**
+ * 狗调用处理程序
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/02/08 09:50:53
+ */
 public class DogInvocationHandler implements InvocationHandler {
 
-	private Object target;
-	public void setTarget(Object target) {
-		this.target = target;
-	}
-	
-	public Object invoke(Object proxy, Method method, Object[] args)
-			throws Throwable {
-		DogAspectj apj = new DogAspectj();
-		//小狗吃东西
-		apj.feed();
-		Object result = method.invoke(target, args);
-		apj.game();
-		return result;
-	}
+    private Object target;
+
+    public void setTarget(Object target) {
+        this.target = target;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        DogAspectj apj = new DogAspectj();
+
+        //小狗吃东西
+        apj.feed();
+        Object result = method.invoke(this.target, args);
+        apj.game();
+
+        return result;
+    }
 }
