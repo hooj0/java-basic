bef7ef39dd7bc728e7ef6b2d06c6851897e608b2
hoojo <hoojo@qq.com>
2020-02-11 12:31:56 +0800

    :sparkles: :bento: :recycle:  Changed 子类

--- src/main/java/io/github/hooj0/generic/generictype/confine/ChildClass.java
+++ src/main/java/io/github/hooj0/generic/generictype/confine/ChildClass.java
@@ -1,56 +1,68 @@
 package io.github.hooj0.generic.generictype.confine;
 
+/**
+ * 子类
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/02/07 17:20:31
+ */
 public class ChildClass<T, F, B, A> implements ParentInterface<A, B> {
-	private T t;//String
-	private F f;//Boolean
-	private A a;//Float
-	private B b;//Integer
-	
-	public B getFoo() {
-		return this.b;
-	}
 
-	public A getToo() {
-		return this.a;
-	}
+    private T t;//String
+    private F f;//Boolean
+    private A a;//Float
+    private B b;//Integer
 
-	public void setFoo(B foo) {
-		System.out.println("B:" + foo.getClass());
-		this.b = foo;
-	}
+    @Override
+    public B getFoo() {
+        return this.b;
+    }
 
-	public void setToo(A too) {
-		System.out.println("A:" + too.getClass());
-		this.a = too;
-	}
+    @Override
+    public A getToo() {
+        return this.a;
+    }
 
-	public T getT() {
-		return t;
-	}
+    @Override
+    public void setFoo(B foo) {
+        System.out.println("B:" + foo.getClass());
+        this.b = foo;
+    }
 
-	public void setT(T t) {
-		System.out.println("t:" + t.getClass());
-		this.t = t;
-	}
+    @Override
+    public void setToo(A too) {
+        System.out.println("A:" + too.getClass());
+        this.a = too;
+    }
 
-	public F getF() {
-		return f;
-	}
+    public T getT() {
+        return this.t;
+    }
 
-	public void setF(F f) {
-		System.out.println("f:" + f.getClass());
-		this.f = f; 
-	}
-	
-	public static void main(String[] args) {
-		ChildClass<String, Boolean, Integer, Float> c = new ChildClass<String, Boolean, Integer, Float>();
-		c.setF(true);
-		Boolean b = c.getF();
-		c.setT("abc");
-		String t = c.getT();
-		c.setFoo(2);
-		Integer foo = c.getFoo();
-		c.setToo(2f);
-		Float too = c.getToo();
-	}
+    public void setT(T t) {
+        System.out.println("t:" + t.getClass());
+        this.t = t;
+    }
+
+    public F getF() {
+        return this.f;
+    }
+
+    public void setF(F f) {
+        System.out.println("f:" + f.getClass());
+        this.f = f;
+    }
+
+    public static void main(String[] args) {
+        ChildClass<String, Boolean, Integer, Float> c = new ChildClass<>();
+        c.setF(true);
+        Boolean b = c.getF();
+        c.setT("abc");
+        String t = c.getT();
+        c.setFoo(2);
+        Integer foo = c.getFoo();
+        c.setToo(2f);
+        Float too = c.getToo();
+    }
 }
