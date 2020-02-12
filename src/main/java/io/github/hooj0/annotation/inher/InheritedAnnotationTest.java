471076a2faca1f9d4fde8f05915b67831b693115
hoojo <hoojo@qq.com>
2020-02-12 10:42:56 +0800

    :sparkles: :bento: :recycle:  Changed 继承了注释测试

--- src/main/java/io/github/hooj0/annotation/inher/InheritedAnnotationTest.java
+++ src/main/java/io/github/hooj0/annotation/inher/InheritedAnnotationTest.java
@@ -1,27 +1,35 @@
 package io.github.hooj0.annotation.inher;
 
+import io.github.hooj0.BasedTests;
 import org.junit.Test;
 
 import java.lang.reflect.Method;
 
+/**
+ * 继承了注释测试
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/02/08 10:01:33
+ */
 @SuppressWarnings("ALL")
-public class InheritedAnnotationTest {
+public class InheritedAnnotationTest extends BasedTests {
 
     @Test
     public void testChildClass() throws NoSuchMethodException {
 	    Class<ChildClass> c = ChildClass.class;
 
-        System.out.println("class");
+        out("class");
         if (c.isAnnotationPresent(InheritedAnnotation.class)) { // true
             InheritedAnnotation ia = c.getAnnotation(InheritedAnnotation.class);
-            System.out.println(ia.value());
+            out(ia.value());
         }
 
-        System.out.println("method");
+        out("method");
         Method m = c.getMethod("method", new Class[]{});
         if (m.isAnnotationPresent(InheritedAnnotation.class)) { // true
             InheritedAnnotation ia = m.getAnnotation(InheritedAnnotation.class);
-            System.out.println(ia.value());
+            out(ia.value());
         }
     }
 
@@ -29,17 +37,17 @@ public class InheritedAnnotationTest {
     public void testChildImpl() throws NoSuchMethodException {
 	    Class<ChildImpl> c = ChildImpl.class;
 
-        System.out.println("class");
+        out("class");
         if (c.isAnnotationPresent(InheritedAnnotation.class)) { // false
             InheritedAnnotation ia = c.getAnnotation(InheritedAnnotation.class);
-            System.out.println(ia.value());
+            out(ia.value());
         }
 
-        System.out.println("method");
+        out("method");
         Method m = c.getMethod("method", new Class[]{});
         if (m.isAnnotationPresent(InheritedAnnotation.class)) { // false
             InheritedAnnotation ia = m.getAnnotation(InheritedAnnotation.class);
-            System.out.println(ia.value());
+            out(ia.value());
         }
     }
 
@@ -47,17 +55,17 @@ public class InheritedAnnotationTest {
     public void testParentClass() throws NoSuchMethodException {
         Class<ParentClass> c = ParentClass.class;
 
-        System.out.println("class");
+        out("class");
         if (c.isAnnotationPresent(InheritedAnnotation.class)) { // true
             InheritedAnnotation ia = c.getAnnotation(InheritedAnnotation.class);
-            System.out.println(ia.value()); // Inherited Annotation
+            out(ia.value()); // Inherited Annotation
         }
 
-        System.out.println("method");
+        out("method");
         Method m = c.getMethod("method", new Class[]{});
         if (m.isAnnotationPresent(InheritedAnnotation.class)) { // true
             InheritedAnnotation ia = m.getAnnotation(InheritedAnnotation.class);
-            System.out.println(ia.value()); // Inherited Annotation
+            out(ia.value()); // Inherited Annotation
         }
     }
 }
