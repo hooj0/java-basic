5cb96db4a5bf89208989b9686bad39b2813571d2
hoojo <hoojo@qq.com>
2020-02-17 10:39:56 +0800

    :sparkles: :bento: :recycle:  Changed 苹果

--- src/main/java/io/github/hooj0/generic/generictype/Apple.java
+++ src/main/java/io/github/hooj0/generic/generictype/Apple.java
@@ -3,8 +3,16 @@ package io.github.hooj0.generic.generictype;
 import java.io.Serializable;
 import java.util.List;
 
+/**
+ * 苹果
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/02/07 17:22:06
+ */
 public class Apple<T extends List & Serializable> {
-	public static void main(String[] args) {
-		//T是继承List的并实现Serializable的
-	}
+
+    public static void main(String[] args) {
+        //T是继承List的并实现Serializable的
+    }
 }
