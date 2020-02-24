3a282982a1c4d4debeb6da7926d1256b4e2f327b
hoojo <hoojo@qq.com>
2020-02-24 21:20:56 +0800

    :sparkles: :bento: :recycle:  Changed 整数情况

--- src/main/java/io/github/hooj0/generic/source/IntegerCase.java
+++ src/main/java/io/github/hooj0/generic/source/IntegerCase.java
@@ -1,6 +1,14 @@
 package io.github.hooj0.generic.source;
 
+/**
+ * 整数情况
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/02/07 17:27:07
+ */
 public class IntegerCase {
+
 	private Integer foo;
 
 	public Integer getFoo() {
