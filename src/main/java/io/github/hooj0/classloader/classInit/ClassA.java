637ff4f23a136ffdf3d141ee406018d0c089904a
hoojo <hoojo@qq.com>
2020-03-14 10:52:56 +0800

    :sparkles: :bento: :recycle:  Changed classa

--- src/main/java/io/github/hooj0/classloader/classInit/ClassA.java
+++ src/main/java/io/github/hooj0/classloader/classInit/ClassA.java
@@ -1,14 +1,23 @@
 package io.github.hooj0.classloader.classInit;
 
+/**
+ * classa
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/02/08 09:09:26
+ */
 @SuppressWarnings("ALL")
 public class ClassA {
-	static {
-		System.out.println("ClassA 静态块一");
-	}
-	public ClassA() {
-		System.out.println("init######ClassA");
-	}
-	static {
-		System.out.println("ClassA 静态块二");
-	}
+    static {
+        System.out.println("ClassA 静态块一");
+    }
+
+    public ClassA() {
+        System.out.println("init######ClassA");
+    }
+
+    static {
+        System.out.println("ClassA 静态块二");
+    }
 }
