43366575466e9cd3e7daefa7e2490787a80dec88
hoojo <hoojo@qq.com>
2020-03-20 21:56:56 +0800

    :sparkles: :bento: :recycle:  Changed 属性试验

--- src/main/java/io/github/hooj0/classloader/field/FieldTest.java
+++ src/main/java/io/github/hooj0/classloader/field/FieldTest.java
@@ -2,25 +2,40 @@ package io.github.hooj0.classloader.field;
 
 import java.lang.reflect.Field;
 
+/**
+ * 属性试验
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/02/08 09:36:20
+ */
 public class FieldTest {
-	public static void main(String[] args) throws SecurityException, Exception {
-		Person p = new Person();
-		Class<Person> clazz = Person.class;
-		//获取Person的name属性,使用getDeclaredField可以获取各种访问控制符的熟悉
-		Field nameField = clazz.getDeclaredField("name");
-		nameField.setAccessible(true);//设置可以访问私有属性
-		nameField.set(p, "java field");
-		Field ageField = clazz.getDeclaredField("age");
-		ageField.setAccessible(true);
-		ageField.set(p, 22);
-		System.out.println(p);
-	}
+
+    public static void main(String[] args) throws Exception {
+        Person p = new Person();
+        Class<Person> clazz = Person.class;
+
+        // 获取Person的name属性,使用getDeclaredField可以获取各种访问控制符的熟悉
+        Field nameField = clazz.getDeclaredField("name");
+        nameField.setAccessible(true);//设置可以访问私有属性
+        nameField.set(p, "java field");
+
+        Field ageField = clazz.getDeclaredField("age");
+        ageField.setAccessible(true);
+        ageField.set(p, 22);
+
+        System.out.println(p);
+    }
 }
 
+
 class Person {
-	private String name;
-	private int age;
-	public String toString() {
-		return "Person [ name: " + name + ", age: " + age + "]";
-	}
+
+    private String name;
+    private int age;
+
+    @Override
+    public String toString() {
+        return "Person [ name: " + this.name + ", age: " + this.age + "]";
+    }
 }
