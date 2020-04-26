5897d1bfd88180e2ab17f277f11d9daa5729f62b
hoojo <hoojo@qq.com>
2020-04-26 12:36:56 +0800

    :sparkles: :bento: :recycle:  Changed 方法和类上的注解都会被继承下来

--- src/main/java/io/github/hooj0/annotation/inher/ChildClass.java
+++ src/main/java/io/github/hooj0/annotation/inher/ChildClass.java
@@ -1,7 +1,11 @@
 package io.github.hooj0.annotation.inher;
 
-/*
+/**
  * 方法和类上的注解都会被继承下来
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/02/08 10:00:56
  */
 @SuppressWarnings("ALL")
 public class ChildClass extends ParentClass {
