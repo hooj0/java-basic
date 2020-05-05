a38529a9eed17e1cca28c726cd40b927514ab19b
hoojo <hoojo@qq.com>
2020-05-05 12:45:56 +0800

    :sparkles: :bento: :recycle:  Changed 父接口

--- src/main/java/io/github/hooj0/generic/generictype/confine/ParentInterface.java
+++ src/main/java/io/github/hooj0/generic/generictype/confine/ParentInterface.java
@@ -1,9 +1,18 @@
 package io.github.hooj0.generic.generictype.confine;
 
+/**
+ * 父接口
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/02/07 17:21:58
+ */
 public interface ParentInterface<T, F> {
-	public void setToo(T too);
-	public void setFoo(F foo);
-	
-	public T getToo();
-	public F getFoo();
+    public void setToo(T too);
+
+    public void setFoo(F foo);
+
+    public T getToo();
+
+    public F getFoo();
 }
