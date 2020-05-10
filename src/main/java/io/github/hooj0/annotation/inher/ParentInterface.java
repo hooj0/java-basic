33e953e9b6c57c61e3af21fbbcfde684b0ada3a0
hoojo <hoojo@qq.com>
2020-05-10 15:46:56 +0800

    :sparkles: :bento: :recycle:  Changed 父接口

--- src/main/java/io/github/hooj0/annotation/inher/ParentInterface.java
+++ src/main/java/io/github/hooj0/annotation/inher/ParentInterface.java
@@ -1,5 +1,12 @@
 package io.github.hooj0.annotation.inher;
 
+/**
+ * 父接口
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/02/08 10:02:04
+ */
 @SuppressWarnings("ALL")
 @InheritedAnnotation
 public interface ParentInterface {
