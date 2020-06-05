4de9c2ecc84425552068f0b123595927eddeffc6
hoojo <hoojo@qq.com>
2020-06-05 14:58:56 +0800

    :sparkles: :bento: :recycle:  Changed 孩子

--- src/main/java/io/github/hooj0/generic/generictype/confine/Child.java
+++ src/main/java/io/github/hooj0/generic/generictype/confine/Child.java
@@ -1,20 +1,28 @@
 package io.github.hooj0.generic.generictype.confine;
 
-public class Child<F, T, H>  extends Parent<T, F>{
-	private H hoo;
+/**
+ * 孩子
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/02/07 17:20:15
+ */
+public class Child<F, T, H> extends Parent<T, F> {
 
-	public H getHoo() {
-		return hoo;
-	}
+    private H hoo;
 
-	public void setHoo(H hoo) {
-		this.hoo = hoo;
-	}
-	
-	public static void main(String[] args) {
-		Child<String, Boolean, Integer> c = new Child<String, Boolean, Integer>();
-		Integer hoo = c.getHoo();
-		String foo = c.getFoo();
-		Boolean too = c.getToo();
-	}
+    public H getHoo() {
+        return this.hoo;
+    }
+
+    public void setHoo(H hoo) {
+        this.hoo = hoo;
+    }
+
+    public static void main(String[] args) {
+        Child<String, Boolean, Integer> c = new Child<>();
+        Integer hoo = c.getHoo();
+        String foo = c.getFoo();
+        Boolean too = c.getToo();
+    }
 }
