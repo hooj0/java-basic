d20514c3030f616215e0dd780be5523e7da78a2a
hoojo <hoojo@qq.com>
2020-06-05 10:12:56 +0800

    :sparkles: :bento: :recycle:  Changed 父

--- src/main/java/io/github/hooj0/generic/generictype/confine/Parent.java
+++ src/main/java/io/github/hooj0/generic/generictype/confine/Parent.java
@@ -1,18 +1,30 @@
 package io.github.hooj0.generic.generictype.confine;
 
+/**
+ * 父
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/02/07 17:21:42
+ */
 public class Parent<T, F> {
-	private T too;
-	private F foo;
-	public T getToo() {
-		return too;
-	}
-	public void setToo(T too) {
-		this.too = too;
-	}
-	public F getFoo() {
-		return foo;
-	}
-	public void setFoo(F foo) {
-		this.foo = foo;
-	}
+
+    private T too;
+    private F foo;
+
+    public T getToo() {
+        return too;
+    }
+
+    public void setToo(T too) {
+        this.too = too;
+    }
+
+    public F getFoo() {
+        return foo;
+    }
+
+    public void setFoo(F foo) {
+        this.foo = foo;
+    }
 }
