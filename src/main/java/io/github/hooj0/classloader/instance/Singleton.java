b630abeb64a5251833867c1ae83babf778706d54
hoojo <hoojo@qq.com>
2020-06-07 18:52:56 +0800

    :sparkles: :bento: :recycle:  Changed 单例

--- src/main/java/io/github/hooj0/classloader/instance/Singleton.java
+++ src/main/java/io/github/hooj0/classloader/instance/Singleton.java
@@ -1,19 +1,30 @@
 package io.github.hooj0.classloader.instance;
 
+/**
+ * 单例
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date Oct 4, 2010 4:16:07 PM
+ */
 public class Singleton {
-	private static Singleton s;
-	//私有无参构造，外部类不能实例化该类
-	private Singleton(){
-		System.out.println("被创建……");
-	}
-	public static Singleton getInstance() {
-		if (s == null) {
-			synchronized (Singleton.class) {
-				if (s == null) {
-					s = new Singleton();
-				}
-			}
-		}
-		return s;
-	}
+
+    private static Singleton s;
+
+    //私有无参构造，外部类不能实例化该类
+    private Singleton() {
+        System.out.println("被创建……");
+    }
+
+    public static Singleton getInstance() {
+        if (s == null) {
+            synchronized (Singleton.class) {
+                if (s == null) {
+                    s = new Singleton();
+                }
+            }
+        }
+
+        return s;
+    }
 }
