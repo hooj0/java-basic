9c95d22f8d0901c1178ae593de54e2840dc589d6
hoojo <hoojo@qq.com>
2020-06-09 12:39:56 +0800

    :sparkles: :bento: :recycle:  Changed 用户

--- src/main/java/io/github/hooj0/classloader/instance/User.java
+++ src/main/java/io/github/hooj0/classloader/instance/User.java
@@ -1,30 +1,44 @@
 package io.github.hooj0.classloader.instance;
 
+/**
+ * 用户
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/02/08 09:46:12
+ */
 public class User {
-	private String name;
-	private String sex;
-	private String age;
-	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getSex() {
-		return sex;
-	}
-	public void setSex(String sex) {
-		this.sex = sex;
-	}
-	public String getAge() {
-		return age;
-	}
-	public void setAge(String age) {
-		this.age = age;
-	}
-	
-	public String toString() {
-		return name + ", " + sex + ", " + age;
-	}
+
+    private String name;
+    private String sex;
+    private String age;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return this.sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAge() {
+        return this.age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + ", " + this.sex + ", " + this.age;
+    }
 }
