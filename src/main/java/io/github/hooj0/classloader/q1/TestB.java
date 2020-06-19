893e4d144c856d86050f1e4e1f6ef6d50b84eb5a
hoojo <hoojo@qq.com>
2020-06-19 12:15:56 +0800

    :sparkles: :bento: :recycle:  Changed 类加载

--- src/main/java/io/github/hooj0/classloader/q1/TestB.java
+++ src/main/java/io/github/hooj0/classloader/q1/TestB.java
@@ -1,17 +1,17 @@
 package io.github.hooj0.classloader.q1;
 
+/**
+ * 类加载
+ * @author hoojo
+ * @version 1.0
+ * @date Sep 27, 2010 10:11:23 PM
+ */
 public class TestB {
 
-	/**
-	 * 
-	 * @author hoojo
-	 * @createDate Sep 27, 2010 10:11:23 PM
-	 * @param args
-	 */
 	@SuppressWarnings("static-access")
 	public static void main(String[] args) {
 		ClassA b = new ClassA();
-		System.out.println(b.a);//10
+		System.out.println(b.a); // 10
 		/**
 		 * 不受TestA的影响，所以结果还是10 
 		 * 因为TestA、TestB运行是2次不同的进程，2次jvm的运行
