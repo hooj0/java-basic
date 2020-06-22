1ca65af7ffad32989bb7f92a2d7ff1861bc1b50d
hoojo <hoojo@qq.com>
2020-06-22 17:41:56 +0800

    :sparkles: :bento: :recycle:  Changed 测试类

--- src/main/java/io/github/hooj0/generic/source/TestUtils.java
+++ src/main/java/io/github/hooj0/generic/source/TestUtils.java
@@ -1,5 +1,12 @@
 package io.github.hooj0.generic.source;
 
+/**
+ * 测试类
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/02/07 17:27:38
+ */
 public abstract class TestUtils {
 	public static String NAME = "abc";
 	
