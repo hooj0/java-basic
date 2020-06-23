4741dc036bc3c74de64ada2cb297e1f6fda338be
hoojo <hoojo@qq.com>
2020-06-23 10:55:56 +0800

    :sparkles: :bento: :recycle:  Changed 引导测试

--- src/main/java/io/github/hooj0/classloader/classLoader/BootstrapTest.java
+++ src/main/java/io/github/hooj0/classloader/classLoader/BootstrapTest.java
@@ -1,17 +1,18 @@
 package io.github.hooj0.classloader.classLoader;
 
-import java.net.URL;
-
 import sun.misc.Launcher;
 
+import java.net.URL;
+
+/**
+ * 引导测试
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date Sep 28, 2010 7:01:07 PM
+ */
 public class BootstrapTest {
 
-	/**
-	 *
-	 * @author hoojo
-	 * @createDate Sep 28, 2010 7:01:07 PM
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		URL[] urls = Launcher.getBootstrapClassPath().getURLs();
 		for (URL u : urls) {
