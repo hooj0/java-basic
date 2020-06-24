9bcdec13b4e47aa9a8b2835e8b5972291d1796c5
hoojo <hoojo@qq.com>
2020-06-24 12:58:56 +0800

    :sparkles: :bento: :recycle:  Changed classb

--- src/main/java/io/github/hooj0/classloader/classInit/ClassB.java
+++ src/main/java/io/github/hooj0/classloader/classInit/ClassB.java
@@ -1,11 +1,19 @@
 package io.github.hooj0.classloader.classInit;
 
+/**
+ * classb
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/02/08 09:09:42
+ */
 @SuppressWarnings("ALL")
 public class ClassB extends ClassA {
-	public ClassB () {
-		System.out.println("############init ClassB");
-	}
-	static {
-		System.out.println("ClassB 静态块一");
-	}
+    public ClassB() {
+        System.out.println("############init ClassB");
+    }
+
+    static {
+        System.out.println("ClassB 静态块一");
+    }
 }
