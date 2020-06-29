25fe4dc23336015d735b4c785fc1edd96de7b663
hoojo <hoojo@qq.com>
2020-06-29 15:59:56 +0800

    :sparkles: :bento: :recycle:  Changed 对象工厂

--- src/main/java/io/github/hooj0/classloader/instance/ObjectFactory.java
+++ src/main/java/io/github/hooj0/classloader/instance/ObjectFactory.java
@@ -2,20 +2,28 @@ package io.github.hooj0.classloader.instance;
 
 import java.util.Date;
 
+/**
+ * 对象工厂
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/02/08 09:41:23
+ */
 public class ObjectFactory {
-	public static Object getInstance(String clazz) {
-		try {
-			Class<?> cls = Class.forName(clazz);
-			return cls.newInstance();
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-	
-	public static void main(String[] args) {
-		//需要强转
-		Date d = (Date) ObjectFactory.getInstance("java.util.Date");
-		System.out.println(d);
-	}
+
+    public static Object getInstance(String clazz) {
+        try {
+            Class<?> cls = Class.forName(clazz);
+            return cls.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        //需要强转
+        Date d = (Date) ObjectFactory.getInstance("java.util.Date");
+        System.out.println(d);
+    }
 }
