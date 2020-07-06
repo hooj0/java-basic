36c47146adea84c031e5fd38c34969a0c91404ad
hoojo <hoojo@qq.com>
2020-07-06 20:21:56 +0800

    :sparkles: :bento: :recycle:  Changed 你好

--- src/main/java/io/github/hooj0/classloader/classLoader/Hello.java
+++ src/main/java/io/github/hooj0/classloader/classLoader/Hello.java
@@ -1,13 +1,14 @@
 package io.github.hooj0.classloader.classLoader;
 
+/**
+ * 你好
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date Sep 28, 2010 10:30:47 PM
+ */
 public class Hello {
 
-	/**
-	 *
-	 * @author hoojo
-	 * @createDate Sep 28, 2010 10:30:47 PM
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		for (String arg : args) {
 			System.out.println("参数：" + arg);
