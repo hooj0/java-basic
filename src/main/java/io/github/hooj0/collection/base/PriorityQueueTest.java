b0d8acb7682dce975229ab558e31e50bacc68248
hoojo <hoojo@qq.com>
2020-07-25 20:17:56 +0800

    :sparkles: :bento: :recycle:  Changed 优先队列测试

--- src/main/java/io/github/hooj0/collection/base/PriorityQueueTest.java
+++ src/main/java/io/github/hooj0/collection/base/PriorityQueueTest.java
@@ -2,13 +2,16 @@ package io.github.hooj0.collection.base;
 
 import java.util.PriorityQueue;
 
+/**
+ * 优先队列测试
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date Jan 12, 2011 9:24:49 PM
+ */
 public class PriorityQueueTest {
 
 	/**
-	 *
-	 * @author hoojo
-	 * @createDate Jan 12, 2011 9:24:49 PM
-	 * @param args
 	 * PriorityQueue保存队列元素的顺序并不是按照加入队列的顺序
 	 * 而是按队列元素的大小进行重新排序，因此当调用peek方法或poll方法
 	 * 来取出队列中的元素时，并不是取出最先进入队列的元素，而是取出队列中最小的元素，
