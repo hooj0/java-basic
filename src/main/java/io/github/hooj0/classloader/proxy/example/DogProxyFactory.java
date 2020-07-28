b2f873fb14a0b62b454aacafc2ac933cc960e171
hoojo <hoojo@qq.com>
2020-07-28 14:02:56 +0800

    :sparkles: :bento: :recycle:  Changed 狗代理工厂

--- src/main/java/io/github/hooj0/classloader/proxy/example/DogProxyFactory.java
+++ src/main/java/io/github/hooj0/classloader/proxy/example/DogProxyFactory.java
@@ -2,10 +2,18 @@ package io.github.hooj0.classloader.proxy.example;
 
 import java.lang.reflect.Proxy;
 
+/**
+ * 狗代理工厂
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/02/08 09:51:47
+ */
 public class DogProxyFactory {
-	public static Object getProxy(Object target) {
-		DogInvocationHandler handler = new DogInvocationHandler();
-		handler.setTarget(target);
-		return Proxy.newProxyInstance(target.getClass().getClassLoader(), target.getClass().getInterfaces(), handler);
-	}
+
+    public static Object getProxy(Object target) {
+        DogInvocationHandler handler = new DogInvocationHandler();
+        handler.setTarget(target);
+        return Proxy.newProxyInstance(target.getClass().getClassLoader(), target.getClass().getInterfaces(), handler);
+    }
 }
