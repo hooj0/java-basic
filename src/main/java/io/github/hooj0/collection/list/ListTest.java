57c8f66cfc85f9b68142311039ceacf317ba79c4
hoojo <hoojo@qq.com>
2020-08-02 12:46:56 +0800

    :sparkles: :bento: :recycle:  Changed 测试列表

--- src/main/java/io/github/hooj0/collection/list/ListTest.java
+++ src/main/java/io/github/hooj0/collection/list/ListTest.java
@@ -1,35 +1,38 @@
 package io.github.hooj0.collection.list;
 
+import io.github.hooj0.BasedTests;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class ListTest {
+/**
+ * 测试列表
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date Jan 9, 2011 7:46:19 PM
+ */
+public class ListTest extends BasedTests {
 
-	/**
-	 * 
-	 * @author hoojo
-	 * @createDate Jan 9, 2011 7:46:19 PM
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		List<String> books = new ArrayList<String>();
 		books.add("java");
 		books.add("Java Web");
 		books.add("Ajax 开发");
 		books.add("struts");
-		System.out.println(books);
+		out(books);
 		
 		books.add(1, "RPC");
 		for (int i = 0; i < books.size(); i++) {
-			System.out.println(books.get(i));
+			out(books.get(i));
 		}
 		
 		books.remove(2);
-		System.out.println(books);
+		out(books);
 		
-		System.out.println(books.indexOf("RPC"));
+		out(books.indexOf("RPC"));
 		books.set(1, "RIA");
-		System.out.println(books);
-		System.out.println(books.subList(1, 3));
+		out(books);
+		out(books.subList(1, 3));
 	}
 }
