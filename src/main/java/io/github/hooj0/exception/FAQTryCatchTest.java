ca68c171b7d40da31a219c0dce4f34764961da0b
hoojo <hoojo@qq.com>
2020-08-14 11:34:56 +0800

    :sparkles: :bento: :recycle:  Changed 异常捕获测试

--- src/main/java/io/github/hooj0/exception/FAQTryCatchTest.java
+++ src/main/java/io/github/hooj0/exception/FAQTryCatchTest.java
@@ -1,5 +1,12 @@
 package io.github.hooj0.exception;
 
+/**
+ * 异常捕获测试
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date Mar 3, 2011 10:50:59 PM
+ */
 public class FAQTryCatchTest {
 
 	public int method() {
@@ -14,14 +21,7 @@ public class FAQTryCatchTest {
 		}
 	}
 	
-	/**
-	 * 
-	 * @author hoojo
-	 * @createDate Mar 3, 2011 10:50:59 PM
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		System.out.println(new FAQTryCatchTest().method());
 	}
-
 }
