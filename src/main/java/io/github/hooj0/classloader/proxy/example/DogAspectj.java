22c37191af6508dc0b5b0144dc05b930193a60b4
hoojo <hoojo@qq.com>
2020-12-01 18:18:56 +0800

    :sparkles: :bento: :recycle:  Changed aspectj

--- src/main/java/io/github/hooj0/classloader/proxy/example/DogAspectj.java
+++ src/main/java/io/github/hooj0/classloader/proxy/example/DogAspectj.java
@@ -1,11 +1,18 @@
 package io.github.hooj0.classloader.proxy.example;
 
+/**
+ * aspectj
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/02/08 09:50:33
+ */
 public class DogAspectj {
-	public void feed() {
-		System.out.println("小狗都要吃东东……");
-	}
-	
-	public void game() {
-		System.out.println("小狗也要玩耍……");
-	}
+    public void feed() {
+        System.out.println("小狗都要吃东东……");
+    }
+
+    public void game() {
+        System.out.println("小狗也要玩耍……");
+    }
 }
