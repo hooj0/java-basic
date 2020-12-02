7487881674a59d4f5627816cc79deb0e2b64731a
hoojo <hoojo@qq.com>
2020-12-02 11:49:56 +0800

    :sparkles: :bento: :recycle:  Changed 标识哈希映射表测试

--- src/main/java/io/github/hooj0/collection/map/IdentityHashMapTest.java
+++ src/main/java/io/github/hooj0/collection/map/IdentityHashMapTest.java
@@ -2,13 +2,16 @@ package io.github.hooj0.collection.map;
 
 import java.util.IdentityHashMap;
 
+/**
+ * 标识哈希映射表测试
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date Jan 13, 2011 10:37:40 PM
+ */
 public class IdentityHashMapTest {
 
 	/**
-	 * 
-	 * @author hoojo
-	 * @createDate Jan 13, 2011 10:37:40 PM
-	 * @param args
 	 * 普通的HashMap比较key相等是比较key的equals
 	 * IdentifyHashMap是比较key的hashCode相等
 	 */
