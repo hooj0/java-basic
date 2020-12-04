490d70caae68f660049e39e3e4e1ce31d20d91f3
hoojo <hoojo@qq.com>
2020-12-04 15:16:56 +0800

    :sparkles: :bento: :recycle:  Changed classa

--- src/main/java/io/github/hooj0/classloader/q1/ClassA.java
+++ src/main/java/io/github/hooj0/classloader/q1/ClassA.java
@@ -1,6 +1,13 @@
 package io.github.hooj0.classloader.q1;
 
+/**
+ * classa
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/02/08 09:53:48
+ */
 public class ClassA {
-	//静态属性a 
+	// 静态属性a
 	public static int a = 10;
 }
