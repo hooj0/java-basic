d44d0efb2e623467c23b2fbad52b64dbb0a47cde
hoojo <hoojo@qq.com>
2020-12-05 12:01:56 +0800

    :sparkles: :bento: :recycle:  Changed 打印学校

--- src/main/java/io/github/hooj0/generic/print/PrintSchool.java
+++ src/main/java/io/github/hooj0/generic/print/PrintSchool.java
@@ -3,14 +3,15 @@ package io.github.hooj0.generic.print;
 
 import io.github.hooj0.generic.enums.Student;
 
+/**
+ * 打印学校
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date Oct 16, 2010 2:21:38 PM
+ */
 public class PrintSchool {
 
-	/**
-	 *
-	 * @author hoojo
-	 * @createDate Oct 16, 2010 2:21:38 PM
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		int i = 0;
 		for (Student s : Student.values()) {
