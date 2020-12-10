f8e0ef9ed6b5af22b17730b457064145901c496a
hoojo <hoojo@qq.com>
2020-12-10 12:43:56 +0800

    :sparkles: :bento: :recycle:  Changed 狗代理测试

--- src/main/java/io/github/hooj0/classloader/proxy/example/DogTest.java
+++ src/main/java/io/github/hooj0/classloader/proxy/example/DogTest.java
@@ -1,15 +1,17 @@
 package io.github.hooj0.classloader.proxy.example;
 
+/**
+ * 狗代理测试
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date Oct 5, 2010 1:26:40 PM
+ */
 public class DogTest {
 
-	/**
-	 *
-	 * @author hoojo
-	 * @createDate Oct 5, 2010 1:26:40 PM
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		Dog dog = new DemiWolf();
+
 		Dog d = (Dog) DogProxyFactory.getProxy(dog);
 		d.info();
 		d.run();
