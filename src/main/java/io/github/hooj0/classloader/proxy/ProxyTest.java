d050ec47a2b3475850f7f147d6045f63a395f9d8
hoojo <hoojo@qq.com>
2020-12-13 21:12:56 +0800

    :sparkles: :bento: :recycle:  Changed 代理测试

--- src/main/java/io/github/hooj0/classloader/proxy/ProxyTest.java
+++ src/main/java/io/github/hooj0/classloader/proxy/ProxyTest.java
@@ -3,26 +3,37 @@ package io.github.hooj0.classloader.proxy;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
 
+/**
+ * 代理测试
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/02/08 09:52:58
+ */
 public class ProxyTest {
-	public static void main(String[] args) {
-		InvocationHandler handler = new MyInvokationHandler();
-		IPerson p = (IPerson) Proxy.newProxyInstance(Person.class.getClassLoader(), Person.class.getInterfaces(), handler);
-		p.walk();
-		p.say("abc");
-	}
+
+    public static void main(String[] args) {
+        InvocationHandler handler = new MyInvocationHandler();
+        IPerson p = (IPerson) Proxy.newProxyInstance(Person.class.getClassLoader(), Person.class.getInterfaces(), handler);
+        p.walk();
+        p.say("abc");
+    }
 }
 
 interface IPerson {
-	public void walk();
-	public void say(String name);
+    public void walk();
+
+    public void say(String name);
 }
 
 class Person implements IPerson {
-	public void walk() {
-		System.out.println("walk……");
-	}
-	
-	public void say(String name) {
-		System.out.println(name + "say....");
-	}
+    @Override
+    public void walk() {
+        System.out.println("walk……");
+    }
+
+    @Override
+    public void say(String name) {
+        System.out.println(name + "say....");
+    }
 }
