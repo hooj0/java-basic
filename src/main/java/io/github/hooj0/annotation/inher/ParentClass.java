42ad2a266e1edcec6427da1960af856ad7e88c53
hoojo <hoojo@qq.com>
2020-12-19 18:39:56 +0800

    :sparkles: :bento: :recycle:  Changed 父类

--- src/main/java/io/github/hooj0/annotation/inher/ParentClass.java
+++ src/main/java/io/github/hooj0/annotation/inher/ParentClass.java
@@ -1,5 +1,12 @@
 package io.github.hooj0.annotation.inher;
 
+/**
+ * 父类
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/02/08 10:01:59
+ */
 @SuppressWarnings("ALL")
 @InheritedAnnotation
 public class ParentClass {
