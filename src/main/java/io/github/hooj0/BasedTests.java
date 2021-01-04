5089d02cb3dbd9064c001084cd880b5bd992cf1e
hoojo <hoojo@qq.com>
2021-01-04 14:34:56 +0800

    :sparkles: :bento: :recycle:  Changed 基础测试

--- src/main/java/io/github/hooj0/BasedTests.java
+++ src/main/java/io/github/hooj0/BasedTests.java
@@ -5,11 +5,15 @@ package io.github.hooj0;
  *
  * @author hoojo
  * @version 1.0
- * @date 2022/01/25 16:48:16
+ * @date 2022/02/07 11:05:49
  */
 public class BasedTests {
 
     public static void out(Object x) {
         System.out.println(x);
+    }
+
+    public static void out() {
+        System.out.println();
     }
 }
