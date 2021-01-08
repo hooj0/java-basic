62956f3498cb1e124c3f9661392246baaaccecdb
hoojo <hoojo@qq.com>
2021-01-08 10:36:57 +0800

    :sparkles: :bento: :tada:  Init add  string case java

---
+++ src/main/java/io/github/hooj0/generic/source/StringCase.java
@@ -0,0 +1,13 @@
+package io.github.hooj0.generic.source;
+
+public class StringCase {
+	private String foo;
+
+	public String getFoo() {
+		return foo;
+	}
+
+	public void setFoo(String foo) {
+		this.foo = foo;
+	}
+}
