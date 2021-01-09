e9388ac59b9b4b58940bc46fc4bb4400dde1ddd5
hoojo <hoojo@qq.com>
2021-01-09 16:57:57 +0800

    :sparkles: :bento: :tada:  Init add <b>function:</b> 杀人的秘诀

---
+++ src/main/java/io/github/hooj0/reflection/proxy/KillPerson.java
@@ -0,0 +1,22 @@
+package io.github.hooj0.reflection.proxy;
+
+/**
+ * <b>function:</b> 杀人的秘诀
+ * @author hoojo
+ * @createDate Oct 6, 2010 5:29:34 PM
+ * @file KillPerson.java
+ * @package com.hoo.base.proxy
+ * @project JavaReflection
+ * @blog http://blog.csdn.net/IBM_hoojo
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public abstract class KillPerson {
+	/**
+	 * <b>function:</b> 杀人的方法
+	 * @author hoojo
+	 * @createDate Oct 6, 2010 5:30:09 PM
+	 * @param name
+	 */
+	public abstract void killPerson(String name);
+}
