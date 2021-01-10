562e057e22ce4a6c71226ba016de5e1f9eefb378
hoojo <hoojo@qq.com>
2021-01-10 18:17:57 +0800

    :sparkles: :bento: :tada:  Init add 	<b>function:</b>

---
+++ src/main/java/io/github/hooj0/classloader/instance/CreateJFrame.java
@@ -0,0 +1,26 @@
+package io.github.hooj0.classloader.instance;
+
+import java.lang.reflect.Constructor;
+
+import javax.swing.JFrame;
+
+public class CreateJFrame {
+
+	/**
+	 * <b>function:</b>
+	 * @author hoojo
+	 * @createDate Oct 4, 2010 4:02:52 PM
+	 * @param args
+	 * @throws ClassNotFoundException 
+	 */
+	public static void main(String[] args) throws Exception {
+		//获取jframe的class对象
+		Class<?> jframeClazz = Class.forName("javax.swing.JFrame");
+		//获取jframe中一个带字符串参数的构造参数
+		Constructor<?> c = jframeClazz.getConstructor(String.class);
+		//用newInstance创建Constructor对象
+		JFrame o = (JFrame) c.newInstance("测试窗口");
+		o.setVisible(true);
+		System.out.println(o);
+	}
+}
