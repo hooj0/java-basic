9a3492b9841d0e6b0387d6ff5facb1230a699efb
hoojo <hoojo@qq.com>
2021-01-14 20:36:58 +0800

    :sparkles: :bento: :tada:  Init add 	<b>function:</b>

---
+++ src/main/java/io/github/hooj0/system/RuntimeTest.java
@@ -0,0 +1,24 @@
+package io.github.hooj0.system;
+
+import java.io.IOException;
+
+
+public class RuntimeTest {
+
+	/**
+	 * <b>function:</b>
+	 * @author hoojo
+	 * @createDate Jan 14, 2011 10:52:01 PM
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		Runtime rt = Runtime.getRuntime();
+		System.out.println("cup count: " + rt.availableProcessors());
+		System.out.println("空闲内存：" + rt.freeMemory());
+		System.out.println("总内存：" + rt.totalMemory());
+		System.out.println("最大可用内存：" + rt.maxMemory());
+		
+		rt.exec("notepad");
+	}
+}
