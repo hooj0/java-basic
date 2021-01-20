d83bba665bd816c3c8b40c300201d977d8cf3bb0
hoojo <hoojo@qq.com>
2021-01-20 18:14:57 +0800

    :sparkles: :bento: :tada:  Init add 	<b>function:</b>

---
+++ src/main/java/io/github/hooj0/generic/enums/EnumTest.java
@@ -0,0 +1,35 @@
+package io.github.hooj0.generic.enums;
+
+public class EnumTest {
+	
+	public static void operation(Tank t) {
+		switch (t) {
+			case TURN_LEFT: 
+				System.out.println("向左转");
+				break;
+			case TURN_RIGHT:
+				System.out.println("向右转");
+				break;
+			case TREAD:
+				System.out.println("行走");
+				break;
+			case FIRE: 
+				System.out.println("开火");
+				break;
+		}
+	}
+	
+	/**
+	 * <b>function:</b>
+	 * @author hoojo
+	 * @createDate Oct 7, 2010 12:11:11 PM
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		operation(Tank.FIRE);
+	}
+}
+
+enum Tank {
+	TURN_LEFT, TURN_RIGHT, FIRE, TREAD;//;号可要可不要
+}
