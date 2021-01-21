d220b75f855ee213184479ba6361e425dc5c6666
hoojo <hoojo@qq.com>
2021-01-21 15:15:58 +0800

    :sparkles: :bento: :truck:  Renamed 文件锁测试

--- src/main/java/io/github/hooj0/io_nio/nio/base/FileLockTest.java
+++ src/main/java/io/github/hooj0/io_nio/nio/base/FileLockTest.java
@@ -1,28 +1,37 @@
-package io.github.hooj0.nio.base;
+package io.github.hooj0.io_nio.nio.base;
+
+import io.github.hooj0.BasedTests;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileLock;
 
-public class FileLockTest {
+/**
+ * 文件锁测试
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/01/26 09:22:27
+ */
+@SuppressWarnings("ALL")
+public class FileLockTest extends BasedTests {
 
 	/**
-	 * <b>function:</b>
 	 * @author hoojo
 	 * @createDate Jan 2, 2011 5:09:56 PM
 	 * @param args
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
 		FileChannel channel = null;
 		try {
 			channel = new FileOutputStream("lock.txt").getChannel();
-			//非阻塞方式对指定文件加锁
+			// 非阻塞方式对指定文件加锁
 			FileLock lock = channel.tryLock();
+
 			Thread.sleep(5000);
-			lock.release();//释放锁
-			
+			lock.release(); // 释放锁
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
