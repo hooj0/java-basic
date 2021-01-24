c0903c404d1d51d6748c057a9d9a814076a4095d
hoojo <hoojo@qq.com>
2021-01-24 09:32:57 +0800

    :sparkles: :bento: :tada:  Init add 	<b>function:</b>

---
+++ src/main/java/io/github/hooj0/generic/generictype/SimpleCollection.java
@@ -0,0 +1,53 @@
+package io.github.hooj0.generic.generictype;
+
+@SuppressWarnings("unchecked")
+public class SimpleCollection<T> {
+	private T[] array;
+	private int index;
+	
+	public SimpleCollection() {
+		array = (T[]) new Object[10];
+	}
+	
+	public SimpleCollection(int length) {
+		array = (T[]) new Object[length];
+	}
+	
+	public void add(T ele) {
+		array[index] = ele;
+		index++;
+	}
+	
+	public T get(int index) {
+		return array[index];
+	}
+	
+	public int length() {
+		return index;
+	}
+	
+	/**
+	 * <b>function:</b>
+	 * @author hoojo
+	 * @createDate Oct 6, 2010 9:34:27 PM
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SimpleCollection<String> arr = new SimpleCollection<String>();
+		for (int i = 0; i < 10; i++) {
+			arr.add(i + "星星");
+		}
+		
+		for (int i = 0; i < arr.length(); i++) {
+			System.out.println(arr.get(i));
+		}
+		
+		SimpleCollection<Integer> arr2 = new SimpleCollection<Integer>();
+		for (int i = 0; i < 10; i++) {
+			arr2.add(i);
+		}
+		for (int i = 0; i < arr2.length(); i++) {
+			System.out.println(arr2.get(i));
+		}
+	}
+}
