2b962f93a5857da972da3506002d391f005ae069
hoojo <hoojo@qq.com>
2021-02-02 10:29:57 +0800

    :sparkles: :bento: :tada:  Init add Description:

---
+++ src/main/java/io/github/hooj0/network/udp/multicast/yk/YeekuProtocol.java
@@ -0,0 +1,19 @@
+package io.github.hooj0.network.udp.multicast.yk;
+
+
+
+/**
+ * Description:
+ * <br/>Copyright (C), 2008-2010, Yeeku.H.Lee
+ * <br/>This program is protected by copyright laws.
+ * <br/>Program Name:
+ * <br/>Date:
+ * @author  Yeeku.H.Lee kongyeeku@163.com
+ * @version  1.0
+ */
+public interface YeekuProtocol
+{
+	String PRESENCE = "⊿⊿";
+	String SPLITTER = "▓";
+}
+
