6c69e4fc655b5418e43b50357c31758d37e11e25
hoojo <hoojo@qq.com>
2021-02-08 17:04:56 +0800

    :sparkles: :bento: :recycle:  Changed 线人的技能，有哪些技术或是特点

--- src/main/java/io/github/hooj0/reflection/dynamicproxy/SkillHelper.java
+++ src/main/java/io/github/hooj0/reflection/dynamicproxy/SkillHelper.java
@@ -1,7 +1,7 @@
 package io.github.hooj0.reflection.dynamicproxy;
 
 /**
- * <b>function:</b> 线人的技能，有哪些技术或是特点
+ *  线人的技能，有哪些技术或是特点
  * @author hoojo
  * @createDate Oct 6, 2010 6:07:57 PM
  * @file SkillHelper.java
@@ -13,28 +13,28 @@ package io.github.hooj0.reflection.dynamicproxy;
  */
 public interface SkillHelper {
 	/**
-	 * <b>function:</b>赢得老大的信任
+	 * 赢得老大的信任
 	 * @author hoojo
 	 * @createDate Oct 6, 2010 6:08:37 PM
 	 */
 	public void gainFaith();
 	
 	/**
-	 * <b>function:</b>个人技能
+	 * 个人技能
 	 * @author hoojo
 	 * @createDate Oct 6, 2010 6:10:16 PM
 	 */
 	public void individualSkill();
 	
 	/**
-	 * <b>function:</b>混入匪窝
+	 * 混入匪窝
 	 * @author hoojo
 	 * @createDate Oct 6, 2010 6:14:10 PM
 	 */
 	public void interfuseDen();
 	
 	/**
-	 * <b>function:</b>拿钱
+	 * 拿钱
 	 * @author hoojo
 	 * @createDate Oct 6, 2010 6:18:16 PM
 	 */
