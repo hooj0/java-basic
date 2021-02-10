afb73e68d1dc7f2491e5b0d208ab5972b206caf3
hoojo <hoojo@qq.com>
2021-02-10 17:24:57 +0800

    :sparkles: :bento: :tada:  Init add <b>function:</b> 收集情报接口

---
+++ src/main/java/io/github/hooj0/reflection/dynamicproxy/GatherInformation.java
@@ -0,0 +1,22 @@
+package io.github.hooj0.reflection.dynamicproxy;
+
+/**
+ * <b>function:</b> 收集情报接口
+ * @author hoojo
+ * @createDate Oct 6, 2010 5:52:55 PM
+ * @file GatherInformation.java
+ * @package com.hoo.base.dynamicproxy
+ * @project JavaReflection
+ * @blog http://blog.csdn.net/IBM_hoojo
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public interface GatherInformation {
+	/**
+	 * <b>function:</b> 收集情报方法
+	 * @author hoojo
+	 * @createDate Oct 6, 2010 5:53:27 PM
+	 * @param msg 情报消息
+	 */
+	public void message(String msg);
+}
