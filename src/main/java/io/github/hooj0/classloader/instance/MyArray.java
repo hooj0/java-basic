ff0524e10007cfa09ef1abb1f40873a67beafe09
hoojo <hoojo@qq.com>
2021-02-11 14:26:57 +0800

    :sparkles: :bento: :tada:  Init add 	<b>function:</b>

---
+++ src/main/java/io/github/hooj0/classloader/instance/MyArray.java
@@ -0,0 +1,27 @@
+package io.github.hooj0.classloader.instance;
+
+import java.lang.reflect.Array;
+
+public class MyArray {
+	
+	@SuppressWarnings("unchecked")
+	public static <T> T[] newInstance(Class<T> componentType, int length) {
+		return (T[]) Array.newInstance(componentType, length);
+	}
+	
+	/**
+	 * <b>function:</b>
+	 * @author hoojo
+	 * @createDate Oct 5, 2010 1:59:23 PM
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String[] arr = MyArray.newInstance(String.class, 10);
+		arr[5] = "abc";
+		int[][] intArr = MyArray.newInstance(int[].class, 10);
+		intArr[1] = new int[] {1, 2};
+		System.out.println(arr[5]);
+		System.out.println(intArr[1][1]);
+		System.out.println(intArr[1][0]);
+	}
+}
