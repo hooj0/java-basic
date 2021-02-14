492488ca4e5534f16ccc70ad97b7be138903f3a1
hoojo <hoojo@qq.com>
2021-02-14 18:07:57 +0800

    :sparkles: :bento: :tada:  Init add <b>function:</b>同步存款

---
+++ src/main/java/io/github/hooj0/thread/account/condition/DepositThread.java
@@ -0,0 +1,29 @@
+package io.github.hooj0.thread.account.condition;
+
+/**
+ * <b>function:</b>同步存款
+ * @author hoojo
+ * @createDate Nov 6, 2010 6:53:30 PM
+ * @file DepositThread.java
+ * @package com.hoo.thread.account.condition
+ * @project JavaThread
+ * @blog http://blog.csdn.net/IBM_hoojo
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class DepositThread extends Thread {
+	private ConditionAccount acc;
+	private double depositMoney;
+	public DepositThread(String name, ConditionAccount acc, double depositMoney) {
+		super(name);
+		this.acc = acc;
+		this.depositMoney = depositMoney;
+	}
+	
+	@Override
+	public void run() {
+		for (int i = 0; i < 100; i++) {
+			acc.deposit(depositMoney);
+		}
+	}
+}
