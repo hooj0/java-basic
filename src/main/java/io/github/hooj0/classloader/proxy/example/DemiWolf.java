c959983c12b5e1002c29279133abe60453456fa7
hoojo <hoojo@qq.com>
2021-02-21 12:07:57 +0800

    :sparkles: :bento: :tada:  Init add  demi wolf java

---
+++ src/main/java/io/github/hooj0/classloader/proxy/example/DemiWolf.java
@@ -0,0 +1,12 @@
+package io.github.hooj0.classloader.proxy.example;
+
+public class DemiWolf implements Dog {
+
+	public void info() {
+		System.out.println("I‘m is a dog！");
+	}
+
+	public void run() {
+		System.out.println("I'm is run！！");
+	}
+}
