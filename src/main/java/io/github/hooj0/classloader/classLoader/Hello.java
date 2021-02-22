a5918b5166339c4bb477860197a73750c0ca2311
hoojo <hoojo@qq.com>
2021-02-22 10:52:57 +0800

    :sparkles: :bento: :tada:  Init add 	<b>function:</b>

---
+++ src/main/java/io/github/hooj0/classloader/classLoader/Hello.java
@@ -0,0 +1,17 @@
+package io.github.hooj0.classloader.classLoader;
+
+public class Hello {
+
+	/**
+	 * <b>function:</b>
+	 * @author hoojo
+	 * @createDate Sep 28, 2010 10:30:47 PM
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		for (String arg : args) {
+			System.out.println("参数：" + arg);
+		}
+	}
+
+}
