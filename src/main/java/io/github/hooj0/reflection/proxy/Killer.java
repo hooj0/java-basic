f9b80077bf6a6ba199f22b659513c05bbf905cb5
hoojo <hoojo@qq.com>
2021-02-24 21:34:57 +0800

    :sparkles: :bento: :tada:  Init add <b>function:</b> 杀手有杀人的本领，他可以随意杀人

---
+++ src/main/java/io/github/hooj0/reflection/proxy/Killer.java
@@ -0,0 +1,24 @@
+package io.github.hooj0.reflection.proxy;
+
+/**
+ * <b>function:</b> 杀手有杀人的本领，他可以随意杀人
+ * @author hoojo
+ * @createDate Oct 6, 2010 5:28:58 PM
+ * @file Killer.java
+ * @package com.hoo.base.proxy
+ * @project JavaReflection
+ * @blog http://blog.csdn.net/IBM_hoojo
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class Killer extends KillPerson {
+	/**
+	 * @see com.hoo.base.proxy.KillPerson#killPerson(String)
+	 * <b>function:</b> 杀手学到了杀人的方法
+	 * @author hoojo
+	 * @createDate Oct 6, 2010 5:30:30 PM
+	 */
+	public void killPerson(String name) {
+		System.out.println("kill people for name is " + name);
+	}
+}
