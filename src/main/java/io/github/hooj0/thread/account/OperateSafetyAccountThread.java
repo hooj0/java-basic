4971019a307e27ff19d8672904be8265777bbdf2
hoojo <hoojo@qq.com>
2021-02-27 15:25:57 +0800

    :sparkles: :bento: :tada:  Init add <b>function:</b>取款操作线程类

---
+++ src/main/java/io/github/hooj0/thread/account/OperateSafetyAccountThread.java
@@ -0,0 +1,29 @@
+package io.github.hooj0.thread.account;
+
+/**
+ * <b>function:</b>取款操作线程类
+ * @author hoojo
+ * @createDate Nov 2, 2010 10:28:25 PM
+ * @file OperateAccountThread.java
+ * @package com.hoo.thread.account
+ * @project JavaThread
+ * @blog http://blog.csdn.net/IBM_hoojo
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class OperateSafetyAccountThread extends Thread {
+	private SafetyAccount acc;
+	private double drawMoney;
+	public OperateSafetyAccountThread(String name, SafetyAccount acc, double drawMoney) {
+		this.setName(name);
+		this.acc = acc;
+		this.drawMoney = drawMoney;
+	}
+	
+	public void run() {
+		/*
+		 * 线程安全方法
+		 */
+		acc.drawMoneyMethod(drawMoney);
+	}
+}
