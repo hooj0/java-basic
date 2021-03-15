dd16f594bfc46f99b1f2b26a5e92dd47abf0ba6d
hoojo <hoojo@qq.com>
2021-03-15 11:11:56 +0800

    :sparkles: :bento: :recycle:  Changed 调用测试

--- src/main/java/io/github/hooj0/reflection/method/InvokeTest.java
+++ src/main/java/io/github/hooj0/reflection/method/InvokeTest.java
@@ -1,8 +1,17 @@
 package io.github.hooj0.reflection.method;
 
+import io.github.hooj0.BasedTests;
+
 import java.lang.reflect.Method;
 
-public class InvokeTest {
+/**
+ * 调用测试
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date Oct 5, 2010 4:13:43 PM
+ */
+public class InvokeTest extends BasedTests {
 	
 	public int add(int first, int last) {
 		return first + last;
@@ -12,24 +21,16 @@ public class InvokeTest {
 		return name + " say: hello world!";
 	}
 	
-	/**
-	 *
-	 * @author hoojo
-	 * @createDate Oct 5, 2010 4:13:43 PM
-	 * @param args
-	 * @throws IllegalAccessException 
-	 * @throws InstantiationException 
-	 */
 	public static void main(String[] args) throws Exception {
 		Class<?> clazz = InvokeTest.class;
 		Object test = clazz.newInstance();
 		//Object test = clazz.getConstructor(new Class[]{}).newInstance(new Object[]{});
 		Method add = clazz.getMethod("add", new Class[]{int.class, int.class});
 		Object addVal = add.invoke(test, new Object[]{new Integer(1), new Integer(2)});
-		System.out.println((Integer)addVal);
+		out((Integer)addVal);
 		
 		Method say = clazz.getMethod("say", new Class[]{String.class});
 		Object sayStr = say.invoke(test, new Object[]{"tom"});
-		System.out.println((String)sayStr);
+		out((String)sayStr);
 	}
 }
