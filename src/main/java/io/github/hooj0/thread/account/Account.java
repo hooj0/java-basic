10d4387f2507ce5863dbcb1bb8a1292a7c759976
hoojo <hoojo@qq.com>
2021-03-16 10:10:56 +0800

    :sparkles: :bento: :recycle:  Changed 取款对象，非线程安全

--- src/main/java/io/github/hooj0/thread/account/Account.java
+++ src/main/java/io/github/hooj0/thread/account/Account.java
@@ -2,46 +2,53 @@ package io.github.hooj0.thread.account;
 
 /**
  * 取款对象，非线程安全
+ *
  * @author hoojo
+ * @version 1.0
  * @createDate Nov 2, 2010 10:28:01 PM
  * @file Account.java
  * @package com.hoo.thread.account
  * @project JavaThread
  * @blog http://blog.csdn.net/IBM_hoojo
  * @email hoojo_@126.com
- * @version 1.0
  */
 public class Account {
-	private String number;
-	private double money;
-	public String getNumber() {
-		return number;
-	}
-	public void setNumber(String number) {
-		this.number = number;
-	}
-	public double getMoney() {
-		return money;
-	}
-	public void setMoney(double money) {
-		this.money = money;
-	}
-	public Account(String number, double money) {
-		this.number = number;
-		this.money = money;
-	}
-	
-	@Override
-	public int hashCode() {
-		return this.number.hashCode();
-	}
-	
-	@Override
-	public boolean equals(Object o) {
-		if (o != null && o.getClass() == Account.class) {
-			Account a = (Account) o;
-			return a.getNumber().equals(number);
-		}
-		return false;
-	}
+
+    private String number;
+    private double money;
+
+    public Account(String number, double money) {
+        this.number = number;
+        this.money = money;
+    }
+
+    public String getNumber() {
+        return this.number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public double getMoney() {
+        return this.money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.number.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o != null && o.getClass() == Account.class) {
+            Account a = (Account) o;
+            return a.getNumber().equals(this.number);
+        }
+        return false;
+    }
 }
