ebfbe4dd6ac0917c17b3e8dccbac7875196dfc81
hoojo <hoojo@qq.com>
2021-03-19 14:03:57 +0800

    :sparkles: :bento: :tada:  Init add 	<b>function:</b>

---
+++ src/main/java/io/github/hooj0/thread/account/Test.java
@@ -0,0 +1,27 @@
+package io.github.hooj0.thread.account;
+
+public class Test {
+
+	/**
+	 * <b>function:</b>
+	 * @author hoojo
+	 * @createDate Oct 31, 2010 6:28:16 PM
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//Account acc = new Account("6554454548521369", 1000D);
+		//模拟取钱
+		//new OperateAccountThread("A", acc, 800).start();
+		//new OperateAccountThread("B", acc, 900).start();
+		
+		//线程安全方法，一样的效果
+		//SafetyAccount sa = new SafetyAccount("6554454548521369", 1000D);
+		//new OperateSafetyAccountThread("A", sa, 800).start();
+		//new OperateSafetyAccountThread("B", sa, 700).start();
+		
+		//带锁资源
+		LockAccount la = new LockAccount("6554454548521369", 1000D);
+		new OperateLockAccountThread("甲", la, 800).start();
+		new OperateLockAccountThread("乙", la, 800).start();
+	}
+}
