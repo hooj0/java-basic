e32ecd366a0bd29ef353f064809a6a7203f7746a
hoojo <hoojo@qq.com>
2021-03-23 21:13:57 +0800

    :sparkles: :bento: :tada:  Init add  user java

---
+++ src/main/java/io/github/hooj0/classloader/instance/User.java
@@ -0,0 +1,30 @@
+package io.github.hooj0.classloader.instance;
+
+public class User {
+	private String name;
+	private String sex;
+	private String age;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	
+	public String toString() {
+		return name + ", " + sex + ", " + age;
+	}
+}
