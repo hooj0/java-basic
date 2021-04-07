bce2d2d5a982bedb5de0ca3b184681d87f28e558
hoojo <hoojo@qq.com>
2021-04-07 12:28:57 +0800

    :sparkles: :bento: :tada:  Init add 	<b>function:</b>

---
+++ src/main/java/io/github/hooj0/generic/staticimport/StaticImport.java
@@ -0,0 +1,19 @@
+package io.github.hooj0.generic.staticimport;
+
+
+//静态导入TestUtils,.*是所有
+import static io.github.hooj0.generic.source.TestUtils.*;
+
+public class StaticImport {
+
+	/**
+	 * <b>function:</b>
+	 * @author hoojo
+	 * @createDate Oct 16, 2010 3:07:56 PM
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(NAME = "bbz");
+		System.out.println(getName());
+	}
+}
