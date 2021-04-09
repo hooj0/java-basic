119c2e4d6f7b0b6687c093cff00edc3dbe54eaa6
hoojo <hoojo@qq.com>
2021-04-09 18:01:57 +0800

    :sparkles: :bento: :tada:  Init add 	<b>function:</b>

---
+++ src/main/java/io/github/hooj0/generic/enumcollection/EnumMapTest.java
@@ -0,0 +1,40 @@
+package io.github.hooj0.generic.enumcollection;
+
+import io.github.hooj0.generic.enums.Student;
+
+import java.util.EnumMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class EnumMapTest {
+
+	/**
+	 * <b>function:</b>
+	 * @author hoojo
+	 * @createDate Oct 16, 2010 2:49:36 PM
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Map<Student, String> map = new EnumMap<Student, String>(Student.class);
+		map.put(Student.TEN, "第十");
+		map.put(Student.ONE, "第一");
+		map.put(Student.TWO, "第二");
+		for (Student s : Student.values()) {
+			System.out.println(map.get(s));
+		}
+		/**
+		 * 从遍访的结果可以看出，对象的順序是根据枚举順序來排列的
+		 */
+		
+		for (String s : map.values()) {
+			System.out.println(s);
+		}
+		
+		Set<Student> set = map.keySet();
+		for (Iterator<Student> iter = set.iterator();iter.hasNext();) {
+			System.out.println(iter.next().getName());
+		}
+	}
+
+}
