30f2d4741c8de97a9b59dc6b1e7521a56316da76
hoojo <hoojo@qq.com>
2021-04-09 12:34:56 +0800

    :sparkles: :bento: :recycle:  Changed 数字格式测试

--- src/main/java/io/github/hooj0/system/NumberFormatTest.java
+++ src/main/java/io/github/hooj0/system/NumberFormatTest.java
@@ -1,43 +1,49 @@
 package io.github.hooj0.system;
 
+import io.github.hooj0.BasedTests;
+
 import java.text.NumberFormat;
 import java.util.Locale;
 
-public class NumberFormatTest {
+/**
+ * 数字格式测试
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date Feb 25, 2011 10:52:13 PM
+ */
+public class NumberFormatTest extends BasedTests {
 
-	/**
-	 * 
-	 * @author hoojo
-	 * @createDate Feb 25, 2011 10:52:13 PM
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		double db = 1234000.567;
 		Locale[] locales = { Locale.CHINA, Locale.JAPAN, Locale.GERMAN, Locale.US };
 		NumberFormat[] nf = new NumberFormat[12];
+
 		for (int i = 0; i < locales.length; i++) {
 			nf[i + 3] = NumberFormat.getNumberInstance(locales[i]);
 			nf[i + 3 + 1] = NumberFormat.getPercentInstance(locales[i]);
 			nf[i + 3 + 2] = NumberFormat.getCurrencyInstance(locales[i]);
 		}
+
 		for (int i = 0; i < locales.length; i++) {
 			switch (i) {
 				case 0:
-					System.out.println("-----中国------");
+					out("-----中国------");
 					break;
 				case 1:
-					System.out.println("-----日本-------");
+					out("-----日本-------");
 					break;
 				case 2:
-					System.out.println("-----德国------");
+					out("-----德国------");
 					break;
 				case 3:
-					System.out.println("-----美国-------");
+					out("-----美国-------");
 					break;
 			}
-			System.out.println("通用价值格式：" + nf[i + 3].format(db));
-			System.out.println("百分比格式：" + nf[i + 3 + 1].format(db));
-			System.out.println("货币格式：" + nf[i + 3 + 2].format(db));
+
+			out("通用价值格式：" + nf[i + 3].format(db));
+			out("百分比格式：" + nf[i + 3 + 1].format(db));
+			out("货币格式：" + nf[i + 3 + 2].format(db));
 		}
 	}
 }
