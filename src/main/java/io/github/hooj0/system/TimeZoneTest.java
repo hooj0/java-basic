75f1d126edcb69f87e5014fdb7b5e3b7f559da67
hoojo <hoojo@qq.com>
2021-04-14 16:10:57 +0800

    :sparkles: :bento: :tada:  Init add 	<b>function:</b>

---
+++ src/main/java/io/github/hooj0/system/TimeZoneTest.java
@@ -0,0 +1,27 @@
+package io.github.hooj0.system;
+
+import java.util.Arrays;
+import java.util.TimeZone;
+
+public class TimeZoneTest {
+
+	/**
+	 * <b>function:</b>
+	 * @author hoojo
+	 * @createDate Feb 14, 2011 10:23:35 PM
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//获取所有时区的id
+		String[] ids = TimeZone.getAvailableIDs();
+		System.out.println(Arrays.toString(ids));
+		
+		TimeZone tz = TimeZone.getDefault();
+		//获取系统默认时区
+		System.out.println(tz.getID());
+		//获取系统默认时区的名称
+		System.out.println(tz.getDisplayName());
+		//获取指定id的时区的名称
+		System.out.println(TimeZone.getTimeZone("CNT").getDisplayName());
+	}
+}
