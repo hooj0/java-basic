f5150b0ad039e946069f014ead950ab52cb54d37
hoojo <hoojo@qq.com>
2021-04-15 11:57:57 +0800

    :sparkles: :bento: :tada:  Init add 	<b>function:</b>

---
+++ src/main/java/io/github/hooj0/io/reader/InputStreamReaderTest.java
@@ -0,0 +1,33 @@
+package io.github.hooj0.io.reader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InputStreamReaderTest {
+
+	/**
+	 * <b>function:</b>
+
+	 * @createDate Dec 13, 2010 12:28:56 AM
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		/*
+		 * System.in包装成BufferedReader，bufferedReader具有一个缓冲功能
+		 * 一次读取一行文本，直到读到换行字才输出数据，没有读到换行将阻塞，直到读到换行符为真
+		 * 
+		 */
+		String buffer = null;
+		while ((buffer = br.readLine()) != null) {
+			if (buffer.equals("exit")) {
+				System.exit(1);
+			}
+			System.out.println("输入内容：" + buffer);
+		}
+		br.close();
+	}
+}
