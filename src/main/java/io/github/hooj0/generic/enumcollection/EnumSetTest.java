39aed103596f04b2878c2ae4431377bb9c2bbd30
hoojo <hoojo@qq.com>
2021-04-15 21:36:57 +0800

    :sparkles: :bento: :tada:  Init add 	<b>function:</b>

---
+++ src/main/java/io/github/hooj0/generic/enumcollection/EnumSetTest.java
@@ -0,0 +1,31 @@
+package io.github.hooj0.generic.enumcollection;
+
+import io.github.hooj0.generic.enums.Student;
+
+import java.util.EnumSet;
+import java.util.Iterator;
+
+public class EnumSetTest {
+
+	/**
+	 * <b>function:</b>
+	 * @author hoojo
+	 * @createDate Oct 16, 2010 2:28:16 PM
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		EnumSet<Student> set = EnumSet.of(Student.EIGHT, Student.NINE);
+		showSet(set);
+		System.out.println("###################");
+		showSet(EnumSet.complementOf(set));//Student.EIGHT, Student.NINE这2个元素就不会出现
+		System.out.println("###################");
+		showSet(EnumSet.range(Student.TWO, Student.SIX));//返回Student.TWO 到 Student.SIX的元素
+	}
+	
+	public static void showSet(EnumSet<Student> set) {
+		Iterator<Student> iter = set.iterator();
+		while (iter.hasNext()) {
+			System.out.println(iter.next().getName());
+		}
+	}
+}
