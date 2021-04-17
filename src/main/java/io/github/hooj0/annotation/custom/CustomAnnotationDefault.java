9ecb1b4e35babd2656337481d7ea8fc01bda4fcb
hoojo <hoojo@qq.com>
2021-04-17 21:10:57 +0800

    :sparkles: :bento: :tada:  Init add <b>function:</b>annotation的默认属性是value

---
+++ src/main/java/io/github/hooj0/annotation/custom/CustomAnnotationDefault.java
@@ -0,0 +1,20 @@
+package io.github.hooj0.annotation.custom;
+
+/**
+ * <b>function:</b>annotation的默认属性是value
+ * @author hoojo
+ * @createDate Oct 30, 2010 2:39:10 PM
+ * @file CustomAnnotationDefault.java
+ * @package com.hoo.annotation.custom
+ * @project JavaAnnotation
+ * @blog http://blog.csdn.net/IBM_hoojo
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+@SuppressWarnings("ALL")
+public @interface CustomAnnotationDefault {
+	String value();
+}
+/*
+ * value是默认属性，在为默认属性赋值的时候可以省略属性名称
+ */
