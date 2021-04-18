9ecfcf0f0836a1c46d915a09a6bec8925db67c66
hoojo <hoojo@qq.com>
2021-04-18 10:57:57 +0800

    :sparkles: :bento: :tada:  Init add 	<b>function:</b>

---
+++ src/main/java/io/github/hooj0/system/I18NResourceBundle.java
@@ -0,0 +1,33 @@
+package io.github.hooj0.system;
+
+import java.text.MessageFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class I18NResourceBundle {
+
+	/**
+	 * <b>function:</b>
+	 * @author hoojo
+	 * @createDate Feb 21, 2011 11:17:49 PM
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Locale[] locales = Locale.getAvailableLocales();
+		for (int i = 0; i < locales.length; i++) {
+			System.out.println(locales[i].getDisplayCountry() + "=" + locales[i].getCountry() + " "
+					+ locales[i].getDisplayLanguage() + "=" + locales[i].getLanguage());
+		}
+		
+		System.out.println();
+		
+		Locale locale = Locale.US;//Locale.getDefault();
+		
+		ResourceBundle bundle = ResourceBundle.getBundle("mess", locale);
+		System.out.println(bundle.getString("hello"));
+		
+		//MessageFormat
+		System.out.println(MessageFormat.format("haha", "zhangsan", new Date()));
+	}
+}
