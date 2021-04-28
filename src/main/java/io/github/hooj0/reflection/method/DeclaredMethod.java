f89a68b47726f72ce355e68bcf1c45731e6e8966
hoojo <hoojo@qq.com>
2021-04-28 18:41:56 +0800

    :sparkles: :bento: :recycle:  Changed 得到某个类的所有方法

--- src/main/java/io/github/hooj0/reflection/method/DeclaredMethod.java
+++ src/main/java/io/github/hooj0/reflection/method/DeclaredMethod.java
@@ -4,7 +4,7 @@ import java.lang.reflect.Method;
 import java.util.Scanner;
 
 /**
- *  得到某个类的所有方法
+ * 得到某个类的所有方法
  * @author hoojo
  * @createDate Oct 5, 2010 3:31:32 PM
  * @file DeclaredMethod.java
@@ -15,10 +15,12 @@ import java.util.Scanner;
  * @version 1.0
  */
 public class DeclaredMethod {
+
 	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(System.in);
 		Class<?> clazz = Class.forName(sc.next());
 		Method[] methods = clazz.getDeclaredMethods();
+
 		for (Method m : methods) {
 			System.out.println(m.toString() + "###" + m.toGenericString());
 		}
