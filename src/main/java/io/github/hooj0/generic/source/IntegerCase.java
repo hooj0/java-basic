f0f59294622c8f886af592f489104d4ce6882161
hoojo <hoojo@qq.com>
2021-05-01 21:50:57 +0800

    :sparkles: :bento: :tada:  Init add  integer case java

---
+++ src/main/java/io/github/hooj0/generic/source/IntegerCase.java
@@ -0,0 +1,13 @@
+package io.github.hooj0.generic.source;
+
+public class IntegerCase {
+	private Integer foo;
+
+	public Integer getFoo() {
+		return foo;
+	}
+
+	public void setFoo(Integer foo) {
+		this.foo = foo;
+	}
+}
