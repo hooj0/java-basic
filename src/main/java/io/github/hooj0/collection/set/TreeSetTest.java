524f6735dd972219996afc067344fb4e880b0322
hoojo <hoojo@qq.com>
2021-05-08 11:39:57 +0800

    :sparkles: :bento: :tada:  Init add 	<b>function:</b>

---
+++ src/main/java/io/github/hooj0/collection/set/TreeSetTest.java
@@ -0,0 +1,39 @@
+package io.github.hooj0.collection.set;
+
+import java.util.Date;
+import java.util.TreeSet;
+
+public class TreeSetTest {
+
+	/**
+	 * <b>function:</b>
+	 * @author hoojo
+	 * @createDate Jan 9, 2011 4:48:46 PM
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		TreeSet<Integer> n = new TreeSet<Integer>();
+		n.add(5);
+		n.add(2);
+		n.add(10);
+		n.add(-9);
+		
+		System.out.println(n);
+		System.out.println(n.first());
+		System.out.println(n.last());
+		//返回小于4的子集
+		System.out.println(n.headSet(4));
+		//返回大于5的子集，如果set中包含5，子集中还包含5
+		System.out.println(n.tailSet(4));
+		//返回大于等于-3，小于4的子集
+		System.out.println(n.subSet(-3, 4));
+		
+		//java.lang.ClassCastException试图向TreeSet中添加不同的对象类型
+		//通过对象的compareTo完成比较
+		TreeSet ts = new TreeSet();
+		//ts.add(new Object());
+		//ts.add(new Object());
+		ts.add("JavaEE");
+		ts.add(new Date());
+	}
+}
