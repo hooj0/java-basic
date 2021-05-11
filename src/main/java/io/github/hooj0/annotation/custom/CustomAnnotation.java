131d1edc7e31adc69f7097ae81dfa346e9d9b474
hoojo <hoojo@qq.com>
2021-05-11 11:41:57 +0800

    :sparkles: :bento: :tada:  Init add <b>function:</b>自定义annotation

---
+++ src/main/java/io/github/hooj0/annotation/custom/CustomAnnotation.java
@@ -0,0 +1,22 @@
+package io.github.hooj0.annotation.custom;
+
+/**
+ * <b>function:</b>自定义annotation
+ * @author hoojo
+ * @createDate Oct 30, 2010 2:29:09 PM
+ * @file CustomAnnotation.java
+ * @package com.hoo.annotation.custom
+ * @project JavaAnnotation
+ * @blog http://blog.csdn.net/IBM_hoojo
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+@SuppressWarnings("ALL")
+public @interface CustomAnnotation {
+
+}
+/*
+ * 自定义annotation类型，只需用在接口关键字前加一个@符号即可
+ * annotation类型默认隐式实现了java.lang.annotation.Annotation接口
+ * 不能再继承或实现接口
+ */
