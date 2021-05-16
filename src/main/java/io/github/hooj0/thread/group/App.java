612bd2d74e4682825b863da08427cefdc37d5f40
hoojo <hoojo@qq.com>
2021-05-16 21:59:56 +0800

    :sparkles: :bento: :truck:  Renamed 应用程序

--- src/main/java/io/github/hooj0/thread/group/App.java
+++ src/main/java/io/github/hooj0/thread/group/App.java
@@ -1,25 +1,30 @@
 package io.github.hooj0.thread.group;
 
-public class Test {
+import io.github.hooj0.BasedTests;
 
-	/**
-	 * 
-	 * @author hoojo
-	 * @createDate Nov 6, 2010 6:57:22 PM
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		//获得主线程所在的线程组，这是所有线程默认的线程组
-		ThreadGroup mainGroup = Thread.currentThread().getThreadGroup();
-		System.out.println("主线程的名称：" + mainGroup.getName());
-		System.out.println("是否后台线程组：" + mainGroup.isDaemon());
-		new CustomGroupThread("主线程组的线程").start();
-		ThreadGroup tg = new ThreadGroup("新线程组-后台线程组");
-		tg.setDaemon(true);
-		System.out.println("新线程组-后台线程组 是否是后台线程" + tg.isDaemon());
-		new CustomGroupThread(tg, "新线程组-后台线程组 的线程A").start();
-		new CustomGroupThread(tg, "新线程组-后台线程组 的线程B").start();
-	}
+/**
+ * 应用程序
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date Nov 6, 2010 6:57:22 PM
+ */
+public class App extends BasedTests {
+
+    public static void main(String[] args) {
+        //获得主线程所在的线程组，这是所有线程默认的线程组
+        ThreadGroup mainGroup = Thread.currentThread().getThreadGroup();
+        out("主线程的名称：" + mainGroup.getName());
+        out("是否后台线程组：" + mainGroup.isDaemon());
+
+        new CustomGroupThread("主线程组的线程").start();
+        ThreadGroup tg = new ThreadGroup("新线程组-后台线程组");
+        tg.setDaemon(true);
+        out("新线程组-后台线程组 是否是后台线程" + tg.isDaemon());
+
+        new CustomGroupThread(tg, "新线程组-后台线程组 的线程A").start();
+        new CustomGroupThread(tg, "新线程组-后台线程组 的线程B").start();
+    }
 }
 /*
  * uncaughtException(Thread t, Throwable e)
