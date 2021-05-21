554a1bd496a8323661f0dac8f3d6793431a868ae
hoojo <hoojo@qq.com>
2021-05-21 15:56:57 +0800

    :sparkles: :bento: :tada:  Init add 	<b>function:</b>

---
+++ src/main/java/io/github/hooj0/io/stream/RuntimeInputStream.java
@@ -0,0 +1,27 @@
+package io.github.hooj0.io.stream;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class RuntimeInputStream {
+
+	/**
+	 * <b>function:</b>
+
+	 * @createDate Dec 13, 2010 1:01:00 PM
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		Process p = Runtime.getRuntime().exec("javac");
+		//p进程错误流创建BufferedReader对象
+		//这个错误流对本程序是输入流，对p则是输出流
+		BufferedReader br = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+		String content = null;
+		while ((content = br.readLine()) != null) {
+			System.out.println(content);
+		}
+		br.close();
+	}
+}
