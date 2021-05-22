2b6ae323fc62a0c0a44f9588425a5e6ec041379f
hoojo <hoojo@qq.com>
2021-05-22 18:01:56 +0800

    :sparkles: :bento: :recycle:  Changed 数组test2

--- src/main/java/io/github/hooj0/reflection/method/ArrayTest2.java
+++ src/main/java/io/github/hooj0/reflection/method/ArrayTest2.java
@@ -1,25 +1,30 @@
 package io.github.hooj0.reflection.method;
 
+import io.github.hooj0.BasedTests;
+
 import java.lang.reflect.Array;
 
-public class ArrayTest2 {
+/**
+ * 数组test2
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date Oct 6, 2010 3:40:30 PM
+ */
+public class ArrayTest2 extends BasedTests {
 
-	/**
-	 *
-	 * @author hoojo
-	 * @createDate Oct 6, 2010 3:40:30 PM
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		int[] doms = {3, 5, 4};
 		Object arr = Array.newInstance(Integer.TYPE, doms);
 		Object arrObj = Array.get(arr, 2);
 		Class<?> clazz = arrObj.getClass().getComponentType();
-		System.out.println(clazz);                                                                  
+		out(clazz);
+
 		Object arrObject = Array.get(arrObj, 4);
-		System.out.println(Array.getLength(arrObject));
+		out(Array.getLength(arrObject));
+
 		Array.set(arrObject, 3, 55);
 		int[][][] array = (int[][][]) arr;
-		System.out.println(array[2][4][3]);
+		out(array[2][4][3]);
 	}
 }
