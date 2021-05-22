0a4f17154dc3fc09eaacea620c6a221c16f7d9a5
hoojo <hoojo@qq.com>
2021-05-22 14:54:57 +0800

    :sparkles: :bento: :tada:  Init add 			%d指向参数i

---
+++ src/main/java/io/github/hooj0/generic/print/Printf.java
@@ -0,0 +1,19 @@
+package io.github.hooj0.generic.print;
+
+import io.github.hooj0.generic.enums.Student;
+
+public class Printf {
+	public static void main(String[] args) {
+		int i = 0;
+		for (Student s : Student.values()) {
+			System.out.printf("%d, \t%s%n", i, s.getName());
+			/**
+			 * %d指向参数i
+			 * \t是tab位
+			 * %s指向参数s.getName()
+			 * %n相当于\n
+			 */
+			++i;
+		}
+	}
+}
