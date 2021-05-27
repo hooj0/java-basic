f3b58c93f76d091710930d7de2c0f5b1ab44e658
hoojo <hoojo@qq.com>
2021-05-27 11:03:56 +0800

    :sparkles: :bento: :recycle:  Changed 线程基本、简单操作

--- src/main/java/io/github/hooj0/thread/base/BaseRunnableThread.java
+++ src/main/java/io/github/hooj0/thread/base/BaseRunnableThread.java
@@ -1,5 +1,8 @@
 package io.github.hooj0.thread.base;
 
+import io.github.hooj0.BasedTests;
+import org.junit.Test;
+
 /**
  * 用接口实现线程，重写run方法
  *
@@ -12,39 +15,48 @@ package io.github.hooj0.thread.base;
  * @blog http://blog.csdn.net/IBM_hoojo
  * @email hoojo_@126.com
  */
-public class BaseRunableThread implements Runnable {
+@SuppressWarnings("ALL")
+public class BaseRunnableThread extends BasedTests implements Runnable {
 
     private int i = 0;
-	
 
+    @Override
     public void run() {
-        for (; i < 100; i++) {
+        for (; this.i < 100; this.i++) {
             /*
              * 实现Runnable接口的方式，需要用Thread.currentThread()来获得当前线程
              * 而继承Thread只需用this就可以获得当前线程
              */
-            System.out.println(Thread.currentThread().getName() + " " + i);
+            out(Thread.currentThread().getName() + " " + this.i);
         }
     }
 
-    /**
-     * @param args
-     * @author hoojo
-     * @createDate Oct 31, 2010 1:23:11 PM
-     */
-    public static void main(String[] args) {
+    @Test
+    public void shareVariableNot() {
+
         for (int i = 0; i < 100; i++) {
-            //调用Thread的currentThread方法获得当前线程
-            System.out.println(Thread.currentThread().getName() + "%%" + i);
+            // 调用Thread的currentThread方法获得当前线程
+            out(Thread.currentThread().getName() + "%%" + i);
             if (i == 20) {
                 //不共享变量
-                //new Thread(new BaseRunableThread(), "无变量共享线程1").start();
-                //new Thread(new BaseRunableThread(), "无变量共享线程2").start();
-                //共享变量，同一个目标对象，运行的run方法体是同一个Thread的方法体
-                //Runnable t = new BaseRunableThread();
-                BaseRunableThread t = new BaseRunableThread();
-                new Thread(t, "变量共享线程1").start();
-                new Thread(t, "变量共享线程2").start();
+                new Thread(new BaseRunnableThread(), "无变量共享线程1").start();
+                new Thread(new BaseRunnableThread(), "无变量共享线程2").start();
+            }
+        }
+    }
+
+    @Test
+    public void shareVariable() {
+
+        for (int i = 0; i < 100; i++) {
+            // 调用Thread的currentThread方法获得当前线程
+            out(Thread.currentThread().getName() + "%%" + i);
+            if (i == 20) {
+                // 共享变量，同一个目标对象，运行的run方法体是同一个Thread的方法体
+                // Runnable thread = new BaseRunnableThread();
+                BaseRunnableThread thread = new BaseRunnableThread();
+                new Thread(thread, "变量共享线程1").start();
+                new Thread(thread, "变量共享线程2").start();
             }
         }
     }
@@ -57,6 +69,15 @@ public class BaseRunableThread implements Runnable {
  * 的run方法。出于这种考虑，Thread类是否可以把任意对象的任意方法作为线程的执行体？
  * Java中是不可以的，Thread必须使用Runnable对象的run方法作为线程的执行体，
  * 但C#却可以做到。
+ * 用接口实现线程最大的区别是：
+ * 接口创建线程可以共享变量
+ * 而用Thread创建线程，每次都需要new一个对象，则不能共享变量
+ * 用接口实现线程最大的区别是：
+ * 接口创建线程可以共享变量
+ * 而用Thread创建线程，每次都需要new一个对象，则不能共享变量
+ * 用接口实现线程最大的区别是：
+ * 接口创建线程可以共享变量
+ * 而用Thread创建线程，每次都需要new一个对象，则不能共享变量
  * 用接口实现线程最大的区别是：
  * 接口创建线程可以共享变量
  * 而用Thread创建线程，每次都需要new一个对象，则不能共享变量
