4a12e4e87a3daf371ea8cf9bcf729f7a050fad1b
hoojo <hoojo@qq.com>
2021-05-28 20:14:56 +0800

    :sparkles: :bento: :recycle:  Changed 复仇者

--- src/main/java/io/github/hooj0/reflection/proxy/Avenger.java
+++ src/main/java/io/github/hooj0/reflection/proxy/Avenger.java
@@ -1,13 +1,14 @@
 package io.github.hooj0.reflection.proxy;
 
+/**
+ * 复仇者
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date Oct 6, 2010 5:44:15 PM
+ */
 public class Avenger {
 
-	/**
-	 * 
-	 * @author hoojo
-	 * @createDate Oct 6, 2010 5:44:15 PM
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		//复仇者想要杀掉伊拉克，但他又不能亲手杀人。只好委托中间人帮他杀掉伊拉克
 		KillPerson kill = new Client();
