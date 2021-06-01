3cdb97e1b89043688ef2c02ffc4677734f7f73ce
hoojo <hoojo@qq.com>
2021-06-01 09:49:57 +0800

    :sparkles: :bento: :tada:  Init add <b>function:</b>

---
+++ src/main/java/io/github/hooj0/collection/base/Season.java
@@ -0,0 +1,16 @@
+package io.github.hooj0.collection.base;
+
+/**
+ * <b>function:</b>
+ * @author hoojo
+ * @createDate Jan 13, 2011 10:55:02 PM
+ * @file Season.java
+ * @package com.hoo.base
+ * @project JavaCollection
+ * @blog http://blog.csdn.net/IBM_hoojo
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public enum Season {
+	SPRING, SUMMER, FALL, WINTER
+}
