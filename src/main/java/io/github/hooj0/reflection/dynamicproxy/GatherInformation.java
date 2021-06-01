ae147b73a1def69695a4d1f901e996baa50fec81
hoojo <hoojo@qq.com>
2021-06-01 09:26:56 +0800

    :sparkles: :bento: :recycle:  Changed 收集情报接口

--- src/main/java/io/github/hooj0/reflection/dynamicproxy/GatherInformation.java
+++ src/main/java/io/github/hooj0/reflection/dynamicproxy/GatherInformation.java
@@ -1,22 +1,25 @@
 package io.github.hooj0.reflection.dynamicproxy;
 
 /**
- *  收集情报接口
+ * 收集情报接口
+ *
  * @author hoojo
+ * @version 1.0
  * @createDate Oct 6, 2010 5:52:55 PM
  * @file GatherInformation.java
  * @package com.hoo.base.dynamicproxy
  * @project JavaReflection
  * @blog http://blog.csdn.net/IBM_hoojo
  * @email hoojo_@126.com
- * @version 1.0
  */
 public interface GatherInformation {
-	/**
-	 *  收集情报方法
-	 * @author hoojo
-	 * @createDate Oct 6, 2010 5:53:27 PM
-	 * @param msg 情报消息
-	 */
-	public void message(String msg);
+
+    /**
+     * 收集情报方法
+     *
+     * @param msg 情报消息
+     * @author hoojo
+     * @createDate Oct 6, 2010 5:53:27 PM
+     */
+    public void message(String msg);
 }
