89c9f7e5b1b71fbe60e7b41fa49658d4f6a87d9b
hoojo <hoojo@qq.com>
2021-06-02 15:42:57 +0800

    :sparkles: :bento: :tada:  Init add 基础测试

---
+++ src/main/java/io/github/hooj0/BasedTests.java
@@ -0,0 +1,15 @@
+package io.github.hooj0;
+
+/**
+ * 基础测试
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/01/25 16:48:16
+ */
+public class BasedTests {
+
+    public static void out(Object x) {
+        System.out.println(x);
+    }
+}
