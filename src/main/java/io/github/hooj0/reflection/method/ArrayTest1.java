5a344856ea384fb59524e2d388d14c1fff827443
hoojo <hoojo@qq.com>
2021-06-05 18:31:56 +0800

    :sparkles: :bento: :recycle:  Changed 数组test1

--- src/main/java/io/github/hooj0/reflection/method/ArrayTest1.java
+++ src/main/java/io/github/hooj0/reflection/method/ArrayTest1.java
@@ -1,27 +1,31 @@
 package io.github.hooj0.reflection.method;
 
+import io.github.hooj0.BasedTests;
+
 import java.lang.reflect.Array;
 
-public class ArrayTest1 {
+/**
+ * 数组test1
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date Oct 6, 2010 3:33:27 PM
+ */
+public class ArrayTest1 extends BasedTests {
 
-	/**
-	 * 
-	 * @author hoojo
-	 * @createDate Oct 6, 2010 3:33:27 PM
-	 * @param args
-	 * @throws ClassNotFoundException 
-	 */
 	public static void main(String[] args) throws Exception {
 		Class<?> clazz = Class.forName("java.lang.String");
 		Object arr = Array.newInstance(clazz, 10);
 		int[] x = new int[]{1, 3, 4};
 		int[] y = {2, 4, 5};
-		System.out.println(x[1] + "," + x[0]);
-		System.out.println(y[2]);
+		out(x[1] + "," + x[0]);
+		out(y[2]);
+
 		Array.set(arr, 5, "array");
 		Array.set(arr, 6, "loader");
-		System.out.println(Array.get(arr, 5));
+		out(Array.get(arr, 5));
+
 		String[] array = (String[]) arr;
-		System.out.println(array[6]);
+		out(array[6]);
 	}
 }
