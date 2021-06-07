930d8c07c62c9ef509c30b71250059cf0d3a6629
hoojo <hoojo@qq.com>
2021-06-07 12:36:57 +0800

    :sparkles: :bento: :tada:  Init add 	<b>function:</b>

---
+++ src/main/java/io/github/hooj0/collection/base/PriorityQueueTest.java
@@ -0,0 +1,33 @@
+package io.github.hooj0.collection.base;
+
+import java.util.PriorityQueue;
+
+public class PriorityQueueTest {
+
+	/**
+	 * <b>function:</b>
+	 * @author hoojo
+	 * @createDate Jan 12, 2011 9:24:49 PM
+	 * @param args
+	 * PriorityQueue保存队列元素的顺序并不是按照加入队列的顺序
+	 * 而是按队列元素的大小进行重新排序，因此当调用peek方法或poll方法
+	 * 来取出队列中的元素时，并不是取出最先进入队列的元素，而是取出队列中最小的元素，
+	 * 先进先出FIFO的原则
+	 */
+	public static void main(String[] args) {
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+		//向队列中加入元素
+		pq.offer(6);
+		pq.offer(-3);
+		pq.offer(9);
+		pq.offer(0);
+		pq.add(2);
+		pq.add(-1);
+		pq.add(4);
+		
+		//输出pq队列，并没有按照元素加入的先后顺序排列 [-3, 0, 9, 6]
+		System.out.println(pq);
+		//访问队列的第一个元素
+		System.out.println(pq.peek());
+	}
+}
