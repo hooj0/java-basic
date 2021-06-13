9b61f6cac68eeaa3b1ebcc4be222b3f759d1b39e
hoojo <hoojo@qq.com>
2021-06-13 20:41:57 +0800

    :sparkles: :bento: :tada:  Init add 	<b>function:</b>

---
+++ src/main/java/io/github/hooj0/annotation/base/SupressWarningsAnnotation.java
@@ -0,0 +1,25 @@
+package io.github.hooj0.annotation.base;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings("ALL")
+public class SupressWarningsAnnotation {
+	
+	/**
+	 * <b>function:</b>
+	 * @author hoojo
+	 * @createDate Oct 28, 2010 10:20:22 PM
+	 * @param args
+	 */
+	@SuppressWarnings({"unchecked", "unused"})
+	public static void main(String[] args) {
+		/*
+		 * @SuppressWarnings是用来压制警告用的，可以是一个或多个（数组）
+		 */
+		@SuppressWarnings("unused")
+		List list = new ArrayList();
+		@SuppressWarnings("unused")
+		String a = "abc";
+	}
+}
