a349b6a50c64126094d4b022c675cde8565508e0
hoojo <hoojo@qq.com>
2021-06-15 18:51:56 +0800

    :sparkles: :bento: :recycle:  Changed 取款操作线程类，操作有锁对象

--- src/main/java/io/github/hooj0/thread/account/OperateLockAccountThread.java
+++ src/main/java/io/github/hooj0/thread/account/OperateLockAccountThread.java
@@ -2,25 +2,29 @@ package io.github.hooj0.thread.account;
 
 /**
  * 取款操作线程类，操作有锁对象
+ *
  * @author hoojo
+ * @version 1.0
  * @createDate Nov 2, 2010 10:28:25 PM
  * @file OperateAccountThread.java
  * @package com.hoo.thread.account
  * @project JavaThread
  * @blog http://blog.csdn.net/IBM_hoojo
  * @email hoojo_@126.com
- * @version 1.0
  */
 public class OperateLockAccountThread extends Thread {
-	private LockAccount acc;
-	private double drawMoney;
-	public OperateLockAccountThread(String name, LockAccount acc, double drawMoney) {
-		this.setName(name);
-		this.acc = acc;
-		this.drawMoney = drawMoney;
-	}
-	
-	public void run() {
-		acc.drawMoneyMethod(drawMoney);
-	}
+
+    private final LockAccount account;
+    private final double drawMoney;
+
+    public OperateLockAccountThread(String name, LockAccount account, double drawMoney) {
+        this.setName(name);
+        this.account = account;
+        this.drawMoney = drawMoney;
+    }
+
+    @Override
+    public void run() {
+	    this.account.drawMoneyMethod(this.drawMoney);
+    }
 }
