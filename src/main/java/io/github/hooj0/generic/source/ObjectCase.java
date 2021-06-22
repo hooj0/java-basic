f3ce9f0c1c651a22707c15ff611598caa4e8662f
hoojo <hoojo@qq.com>
2021-06-22 11:49:57 +0800

    :sparkles: :bento: :tada:  Init add  object case java

---
+++ src/main/java/io/github/hooj0/generic/source/ObjectCase.java
@@ -0,0 +1,13 @@
+package io.github.hooj0.generic.source;
+
+public class ObjectCase {
+	private Object foo;
+
+	public Object getFoo() {
+		return foo;
+	}
+
+	public void setFoo(Object foo) {
+		this.foo = foo;
+	}
+}
