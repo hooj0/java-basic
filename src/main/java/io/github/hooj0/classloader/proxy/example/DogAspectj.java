af51ca957ae78ec21edceec555f8d003ac63848d
hoojo <hoojo@qq.com>
2021-06-26 10:43:57 +0800

    :sparkles: :bento: :tada:  Init add  dog aspectj java

---
+++ src/main/java/io/github/hooj0/classloader/proxy/example/DogAspectj.java
@@ -0,0 +1,11 @@
+package io.github.hooj0.classloader.proxy.example;
+
+public class DogAspectj {
+	public void feed() {
+		System.out.println("小狗都要吃东东……");
+	}
+	
+	public void game() {
+		System.out.println("小狗也要玩耍……");
+	}
+}
