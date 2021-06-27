53e53f6e26062bcd6ad4059165f2f07fef95d927
hoojo <hoojo@qq.com>
2021-06-27 18:21:57 +0800

    :sparkles: :bento: :tada:  Init add 学生

---
+++ src/main/java/io/github/hooj0/annotation/Student.java
@@ -0,0 +1,25 @@
+package io.github.hooj0.annotation;
+
+/**
+ * 学生
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/01/25 16:37:55
+ */
+public class Student {
+	private String name;
+	private int age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
