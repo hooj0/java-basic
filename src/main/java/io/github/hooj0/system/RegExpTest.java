78a861c4cf5464d8c9a98c534c43ccdea2657aab
hoojo <hoojo@qq.com>
2021-06-29 16:17:56 +0800

    :sparkles: :bento: :recycle:  Changed reg实验测试

--- src/main/java/io/github/hooj0/system/RegExpTest.java
+++ src/main/java/io/github/hooj0/system/RegExpTest.java
@@ -1,38 +1,42 @@
 package io.github.hooj0.system;
 
+import io.github.hooj0.BasedTests;
+
 import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class RegExpTest {
+/**
+ * reg实验测试
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date Feb 15, 2011 10:33:53 PM
+ */
+@SuppressWarnings("ALL")
+public class RegExpTest extends BasedTests {
 
-	/**
-	 * 
-	 * @author hoojo
-	 * @createDate Feb 15, 2011 10:33:53 PM
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		String str = "hello , java!";
 		//贪婪模式的正则表达式
-		System.out.println(str.replaceFirst("\\w*", "~"));;
+		out(str.replaceFirst("\\w*", "~"));;
 		//勉强模式的正则表达式
-		System.out.println(str.replaceFirst("\\w*?", "~"));
+		out(str.replaceFirst("\\w*?", "~"));
 		
 		//将一个字符串编译成Pattern对象
 		Pattern p = Pattern.compile("a*b");
 		//使用Pattern对象创建Matcher对象
 		Matcher m = p.matcher("aaaaaab");
 		boolean b = m.matches();//返回true
-		System.out.println(b);
+		out(b);
 		
 		//和上面效果一样
 		b = Pattern.matches("a*b", "aaaaab");
-		System.out.println(b);
+		out(b);
 		
 		m = Pattern.compile("\\w+").matcher("Java is very easy!");
 		while (m.find()) {
-			System.out.println(m.group());
+			out(m.group());
 		}
 		int i = 0;
 		while (m.find(i)) {
@@ -41,10 +45,10 @@ public class RegExpTest {
 		}
 		
 		str = "Java is very easy";
-		System.out.println("目标是：" + str);
+		out("目标是：" + str);
 		m = Pattern.compile("\\w+").matcher(str);
 		while (m.find()) {
-			System.out.println(m.group() + "子串的起始位置：" + m.start() + ", 其结束位置： " + m.end());
+			out(m.group() + "子串的起始位置：" + m.start() + ", 其结束位置： " + m.end());
 		}
 		
 		String[] mails = {
@@ -64,9 +68,9 @@ public class RegExpTest {
 				matcher.reset(mail);
 			}
 			if (matcher.matches()) {
-				System.out.println(mail + "有效邮箱！");
+				out(mail + "有效邮箱！");
 			} else {
-				System.out.println(mail + "无效邮箱！");
+				out(mail + "无效邮箱！");
 			}
 		}
 		
@@ -83,12 +87,12 @@ public class RegExpTest {
 			} else {
 				matcher2.reset(strs[i]);
 			}
-			System.out.println(matcher2.replaceAll("哈哈\2"));
+			out(matcher2.replaceAll("哈哈\2"));
 		}
 		
 		for (String s : strs) {
-			System.out.println(s.replaceFirst("re\\w*", "哈哈\1"));
-			System.out.println(Arrays.toString(s.split(" ")));
+			out(s.replaceFirst("re\\w*", "哈哈\1"));
+			out(Arrays.toString(s.split(" ")));
 		}
 	}
 }
