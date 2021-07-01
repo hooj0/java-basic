7ec06b58cad71a2834f8620bc663e1c633f2b93d
hoojo <hoojo@qq.com>
2021-07-01 20:08:57 +0800

    :sparkles: :bento: :tada:  Init add 	<b>function:</b>

---
+++ src/main/java/io/github/hooj0/reflection/getclass/ClassTest.java
@@ -0,0 +1,33 @@
+package io.github.hooj0.reflection.getclass;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ClassTest {
+
+	/**
+	 * <b>function:</b>
+	 * @author hoojo
+	 * @createDate Oct 6, 2010 2:43:30 PM
+	 * @param args
+	 * @throws Exception 
+	 */
+	public static void main(String[] args) throws Exception {
+		Class<?> clazz1 = Class.forName("java.util.Date");
+		System.out.println(clazz1);
+		Class<?> clazz2 = Date.class;
+		Class<?> c = int[].class;
+		System.out.println(c);
+		System.out.println(clazz2);
+		String name = "abcd";
+		Class<?> clazz3 = name.getClass();
+		System.out.println(clazz3);
+		Class<?> clazz4 = SimpleDateFormat.class.getSuperclass();
+		System.out.println(clazz4);
+		Class<?> clazz5 = Boolean.TYPE;
+		Class<?> clazz6 = Boolean.class;
+		System.out.println((clazz5 == clazz6) + ":" + clazz5 + "," + clazz6);
+		Class<?> clazz7 = ClassLoader.getSystemClassLoader().loadClass("java.util.Date");
+		System.out.println(clazz7);
+	}
+}
