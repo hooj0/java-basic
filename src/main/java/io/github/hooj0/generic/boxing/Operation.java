533ff724d92c8f3f63595c441e902b055f96746f
hoojo <hoojo@qq.com>
2021-07-05 17:06:57 +0800

    :sparkles: :bento: :tada:  Init add 		a == b

---
+++ src/main/java/io/github/hooj0/generic/boxing/Operation.java
@@ -0,0 +1,28 @@
+package io.github.hooj0.generic.boxing;
+
+public class Operation {
+	public static void main(String[] args) {
+		Integer a = 100;//在-127~128范围，相当于int
+		Integer b = 100;
+		if (a == b) {
+			System.out.println("a == b");
+		} else {
+			System.out.println("a != b");
+		}
+		
+		Integer one = 150;//大于128，相当于new Integer
+		Integer two = 150;
+		if (one == two) {
+			System.out.println("one == two");
+		} else {
+			System.out.println("one != two");
+		}
+		
+		/**
+		 * a == b
+		 * one != two
+		 * ??? 原因是：在装箱和拆箱中，Integer的值在-127——128就比较值（相当于int比较）
+		 * 当超出这个范围就比较内存地址，也就是对象间的比较
+		 */
+	}
+}
