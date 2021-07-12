076e1d8e84ac1f5b5d220b04a7d1ddb2dc764af6
hoojo <hoojo@qq.com>
2021-07-12 14:44:57 +0800

    :sparkles: :bento: :tada:  Init add 	<b>function:</b>

---
+++ src/main/java/io/github/hooj0/collection/list/ListTest2.java
@@ -0,0 +1,34 @@
+package io.github.hooj0.collection.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListTest2 {
+
+	/**
+	 * <b>function:</b>
+	 * @author hoojo
+	 * @createDate Jan 9, 2011 9:20:21 PM
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		List<Object> list = new ArrayList<Object>();
+		list.add("struts");
+		list.add("spring");
+		list.add("hibernate");
+		System.out.println(list);
+		//equals为true就删除一个元素
+		list.remove(new A());
+		System.out.println(list);
+		
+		list.remove(new A());
+		System.out.println(list);
+	}
+}
+
+class A {
+	@Override
+	public boolean equals(Object obj) {
+		return true;
+	}
+}
