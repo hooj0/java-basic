71e2fe99ca5ba82e20b196974030e0ef4a8a6578
hoojo <hoojo@qq.com>
2021-07-13 14:15:57 +0800

    :sparkles: :bento: :tada:  Init add <b>function:</b> 线人的技能，有哪些技术或是特点

---
+++ src/main/java/io/github/hooj0/reflection/dynamicproxy/SkillHelper.java
@@ -0,0 +1,42 @@
+package io.github.hooj0.reflection.dynamicproxy;
+
+/**
+ * <b>function:</b> 线人的技能，有哪些技术或是特点
+ * @author hoojo
+ * @createDate Oct 6, 2010 6:07:57 PM
+ * @file SkillHelper.java
+ * @package com.hoo.base.dynamicproxy
+ * @project JavaReflection
+ * @blog http://blog.csdn.net/IBM_hoojo
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public interface SkillHelper {
+	/**
+	 * <b>function:</b>赢得老大的信任
+	 * @author hoojo
+	 * @createDate Oct 6, 2010 6:08:37 PM
+	 */
+	public void gainFaith();
+	
+	/**
+	 * <b>function:</b>个人技能
+	 * @author hoojo
+	 * @createDate Oct 6, 2010 6:10:16 PM
+	 */
+	public void individualSkill();
+	
+	/**
+	 * <b>function:</b>混入匪窝
+	 * @author hoojo
+	 * @createDate Oct 6, 2010 6:14:10 PM
+	 */
+	public void interfuseDen();
+	
+	/**
+	 * <b>function:</b>拿钱
+	 * @author hoojo
+	 * @createDate Oct 6, 2010 6:18:16 PM
+	 */
+	public void getMoney();
+}
