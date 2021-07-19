98214fd9bfea91667414173670b2046753663b27
hoojo <hoojo@qq.com>
2021-07-19 18:01:57 +0800

    :sparkles: :bento: :truck:  Renamed 字符集转换测试

--- src/main/java/io/github/hooj0/io_nio/nio/charset/CharsetTransformTest.java
+++ src/main/java/io/github/hooj0/io_nio/nio/charset/CharsetTransformTest.java
@@ -1,4 +1,6 @@
-package io.github.hooj0.nio.charset;
+package io.github.hooj0.io_nio.nio.charset;
+
+import io.github.hooj0.BasedTests;
 
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
@@ -7,10 +9,17 @@ import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
 import java.nio.charset.CharsetEncoder;
 
-public class CharsetTransformTest {
+/**
+ * 字符集转换测试
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/01/26 09:15:18
+ */
+@SuppressWarnings("ALL")
+public class CharsetTransformTest extends BasedTests {
 
 	/**
-	 * <b>function:</b>
 	 * @author hoojo
 	 * @createDate Jan 2, 2011 4:56:43 PM
 	 * @param args
@@ -18,8 +27,10 @@ public class CharsetTransformTest {
 	 */
 	public static void main(String[] args) throws CharacterCodingException {
 		Charset charset = Charset.forName("GBK");
+
 		CharsetDecoder decoder = charset.newDecoder();
 		CharsetEncoder encoder = charset.newEncoder();
+
 		CharBuffer buffer = CharBuffer.allocate(8);
 		buffer.put("a");
 		buffer.put("b");
@@ -28,8 +39,8 @@ public class CharsetTransformTest {
 		
 		ByteBuffer b = encoder.encode(buffer);
 		for (int i = 0; i < b.limit(); i++) {
-			System.out.println(b.get(i) + " ");
+			System.out.print(b.get(i) + " ");
 		}
-		System.out.println(decoder.decode(b));
+		out(decoder.decode(b));
 	}
 }
