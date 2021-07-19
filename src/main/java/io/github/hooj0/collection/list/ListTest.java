baf99d8d0e0dd7cc6b4898d375c88391acb2af3a
hoojo <hoojo@qq.com>
2021-07-19 16:56:57 +0800

    :sparkles: :bento: :tada:  Init add 	<b>function:</b>

---
+++ src/main/java/io/github/hooj0/collection/list/ListTest.java
@@ -0,0 +1,35 @@
+package io.github.hooj0.collection.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListTest {
+
+	/**
+	 * <b>function:</b>
+	 * @author hoojo
+	 * @createDate Jan 9, 2011 7:46:19 PM
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		List<String> books = new ArrayList<String>();
+		books.add("java");
+		books.add("Java Web");
+		books.add("Ajax 开发");
+		books.add("struts");
+		System.out.println(books);
+		
+		books.add(1, "RPC");
+		for (int i = 0; i < books.size(); i++) {
+			System.out.println(books.get(i));
+		}
+		
+		books.remove(2);
+		System.out.println(books);
+		
+		System.out.println(books.indexOf("RPC"));
+		books.set(1, "RIA");
+		System.out.println(books);
+		System.out.println(books.subList(1, 3));
+	}
+}
