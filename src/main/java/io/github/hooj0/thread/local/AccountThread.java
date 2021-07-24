ba7c580831446858fd8fe902a98c7fbcbd88cbee
hoojo <hoojo@qq.com>
2021-07-24 11:32:56 +0800

    :sparkles: :bento: :recycle:  Changed 账户线程

--- src/main/java/io/github/hooj0/thread/local/AccountThread.java
+++ src/main/java/io/github/hooj0/thread/local/AccountThread.java
@@ -10,20 +10,20 @@ package io.github.hooj0.thread.local;
 public class AccountThread extends Thread {
 
     /** 定义一个Account属性 */
-    private final Account acc;
+    private final Account account;
 
-    public AccountThread(Account acc, String name) {
+    public AccountThread(Account account, String name) {
         this.setName(name);
-        this.acc = acc;
+        this.account = account;
     }
 
     @Override
     public void run() {
         for (int i = 0; i < 10; i++) {
             if (i == 6) {
-	            this.acc.setName(this.getName());
+	            this.account.setName(this.getName());
             }
-            System.out.println(this.acc.getName() + " 账户的i值：" + i);
+            System.out.println(this.account.getName() + " 账户的i值：" + i);
         }
     }
 }
