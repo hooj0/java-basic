8c9c1c5ec806c47a09ce33dc4226998c179bb6a4
hoojo <hoojo@qq.com>
2021-08-02 11:07:56 +0800

    :sparkles: :bento: :truck:  Renamed 自定义异常处理程序

--- src/main/java/io/github/hooj0/thread/group/CustomExceptionHandler.java
+++ src/main/java/io/github/hooj0/thread/group/CustomExceptionHandler.java
@@ -1,16 +1,26 @@
 package io.github.hooj0.thread.group;
 
+import io.github.hooj0.BasedTests;
+
 import java.lang.Thread.UncaughtExceptionHandler;
 
-public class TestExceptionHandler implements UncaughtExceptionHandler {
+/**
+ * 自定义异常处理程序
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/01/26 15:02:20
+ */
+public class CustomExceptionHandler extends BasedTests implements UncaughtExceptionHandler {
 
-	public void uncaughtException(Thread t, Throwable e) {
-		System.out.println(t.getName() + " 发生异常：" + e + "\n" + e.getMessage());
-	}
-	
-	public static void main(String[] args) {
-		Thread.currentThread().setUncaughtExceptionHandler(new TestExceptionHandler());
-		@SuppressWarnings("unused")
-		int a = 5 / 0;
-	}
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        out(t.getName() + " 发生异常：" + e + "\n" + e.getMessage());
+    }
+
+    public static void main(String[] args) {
+        Thread.currentThread().setUncaughtExceptionHandler(new CustomExceptionHandler());
+        @SuppressWarnings("unused")
+        int a = 5 / 0;
+    }
 }
