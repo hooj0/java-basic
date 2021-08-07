b67b8bdaf4f2a95fdf1787f51d4898c6c3c3b324
hoojo <hoojo@qq.com>
2021-08-07 21:06:56 +0800

    :sparkles: :bento: :recycle:  Changed 线程输入流

--- src/main/java/io/github/hooj0/thread/stream/ReaderThread.java
+++ src/main/java/io/github/hooj0/thread/stream/ReaderThread.java
@@ -13,30 +13,31 @@ import java.io.PipedReader;
  * @blog http://blog.csdn.net/IBM_hoojo
  * @email hoojo_@126.com
  */
+@SuppressWarnings("ALL")
 public class ReaderThread extends Thread {
 
-    //private PipedReader pr;
+    // private PipedReader reader;
     /** 创建包装管道流的Buffer对象 */
-    private final BufferedReader br;
+    private final BufferedReader reader;
 
-    public ReaderThread(PipedReader pr) {
-        //this.pr = pr;
-        this.br = new BufferedReader(pr);
+    public ReaderThread(PipedReader reader) {
+        //this.reader = reader;
+        this.reader = new BufferedReader(reader);
     }
 
     @Override
     public void run() {
         String bf = null;
         try {
-            while ((bf = this.br.readLine()) != null) {
+            while ((bf = this.reader.readLine()) != null) {
                 System.out.println(bf);
             }
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             try {
-                if (this.br != null) {
-                    this.br.close();
+                if (this.reader != null) {
+                    this.reader.close();
                 }
             } catch (Exception e) {
                 e.printStackTrace();
