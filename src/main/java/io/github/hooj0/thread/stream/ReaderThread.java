3a3e393f6b1b290dabe729c9bf1c930e277570ea
hoojo <hoojo@qq.com>
2021-08-08 17:58:56 +0800

    :sparkles: :bento: :recycle:  Changed 线程输入流

--- src/main/java/io/github/hooj0/thread/stream/ReaderThread.java
+++ src/main/java/io/github/hooj0/thread/stream/ReaderThread.java
@@ -5,41 +5,42 @@ import java.io.PipedReader;
 
 /**
  * 线程输入流
+ *
  * @author hoojo
+ * @version 1.0
  * @createDate Nov 6, 2010 6:49:05 PM
  * @file ReaderThread.java
- * @package com.hoo.thread.stream
- * @project JavaThread
  * @blog http://blog.csdn.net/IBM_hoojo
  * @email hoojo_@126.com
- * @version 1.0
  */
 public class ReaderThread extends Thread {
-	//private PipedReader pr;
-	//创建包装管道流的Buffer对象
-	private BufferedReader br;
-	
-	public ReaderThread(PipedReader pr) {
-		//this.pr = pr;
-		this.br = new BufferedReader(pr);
-	}
-	
-	public void run() {
-		String bf = null;
-		try {
-			while ((bf = br.readLine()) != null) {
-				System.out.println(bf);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (br != null) {
-					br.close();
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
+
+    //private PipedReader pr;
+    /** 创建包装管道流的Buffer对象 */
+    private final BufferedReader br;
+
+    public ReaderThread(PipedReader pr) {
+        //this.pr = pr;
+        this.br = new BufferedReader(pr);
+    }
+
+    @Override
+    public void run() {
+        String bf = null;
+        try {
+            while ((bf = this.br.readLine()) != null) {
+                System.out.println(bf);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (this.br != null) {
+                    this.br.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
