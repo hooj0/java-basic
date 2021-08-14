810f597aa95883862e90009209bda3470c7282de
hoojo <hoojo@qq.com>
2021-08-14 21:33:57 +0800

    :sparkles: :bento: :tada:  Init add 	<b>function:</b>

---
+++ src/main/java/io/github/hooj0/collection/set/EnumSetTest.java
@@ -0,0 +1,33 @@
+package io.github.hooj0.collection.set;
+
+import io.github.hooj0.collection.base.Season;
+
+import java.util.EnumSet;
+
+public class EnumSetTest {
+
+	/**
+	 * <b>function:</b>
+	 * @author hoojo
+	 * @createDate Jan 9, 2011 6:22:01 PM
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		EnumSet<Season> e = EnumSet.allOf(Season.class);
+		System.out.println(e);
+		EnumSet<Season> e2 = EnumSet.noneOf(Season.class);
+		System.out.println(e2);
+		
+		e2.add(Season.WINTER);
+		e2.add(Season.SPRING);
+		System.out.println(e2);
+		
+		EnumSet<Season> e3 = EnumSet.of(Season.SUMMER, Season.WINTER);
+		System.out.println(e3);
+		EnumSet<Season> e4 = EnumSet.range(Season.SUMMER, Season.WINTER);
+		System.out.println(e4);
+		
+		EnumSet<Season> e5 = EnumSet.complementOf(e4);
+		System.out.println(e5);
+	}
+}
