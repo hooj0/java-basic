e253ad45a9868a1e1462900c5f7d93e1bb2b13a5
hoojo <hoojo@qq.com>
2021-08-15 15:21:57 +0800

    :sparkles: :bento: :tada:  Init add <b>function:</b>线程优先级

---
+++ src/main/java/io/github/hooj0/thread/base/PriorityThread.java
@@ -0,0 +1,45 @@
+package io.github.hooj0.thread.base;
+
+/**
+ * <b>function:</b>线程优先级
+ * @author hoojo
+ * @createDate Nov 6, 2010 6:39:40 PM
+ * @file PriorityThread.java
+ * @package com.hoo.thread.base
+ * @project JavaThread
+ * @blog http://blog.csdn.net/IBM_hoojo
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class PriorityThread extends Thread {
+
+	public PriorityThread(String name) {
+		this.setName(name);
+	}
+	
+	@Override
+	public void run() {
+		for (int i = 0; i < 50; i++) {
+			System.out.println(getName() + ", " + i + " ,优先级：" + this.getPriority());
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(Thread.currentThread().getPriority());
+		Thread.currentThread().setPriority(6);
+		for (int i = 0; i < 30; i++) {
+			if (i == 10) {
+				PriorityThread pt = new PriorityThread("低级");
+				pt.start();
+				System.out.println(pt.getName() + " 创建优先级：" + pt.getPriority());
+				pt.setPriority(Thread.MIN_PRIORITY);
+			}
+			if (i == 20) {
+				PriorityThread pt = new PriorityThread("高级");
+				pt.start();
+				System.out.println(pt.getName() + " 创建优先级：" + pt.getPriority());
+				pt.setPriority(Thread.MAX_PRIORITY);
+			}
+		}
+	}
+}
