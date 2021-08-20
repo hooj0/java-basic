2fae9d7c4f446630f13bfe8721fdb58573461f78
hoojo <hoojo@qq.com>
2021-08-20 17:53:56 +0800

    :sparkles: :bento: :truck:  Renamed 自定义协议

--- src/main/java/io/github/hooj0/network/udp/multicast/chart/CustomProtocol.java
+++ src/main/java/io/github/hooj0/network/udp/multicast/chart/CustomProtocol.java
@@ -1,16 +1,15 @@
 package io.github.hooj0.network.udp.multicast.chart;
 
 /**
- * constom协议
+ * 自定义协议
  *
  * @author hoojo
  * @version 1.0
- * @date 2022/02/07 14:56:55
+ * @date 2022/02/07 14:57:35
  */
-public final class ConstomProtocol {
+public final class CustomProtocol {
 	//定义协议字符串的长度
 	public static int PROTOCOL_LENGHT = 2;
 	public static String PRESENCE = "★【";
 	public static String SPLITTER = "※";
-	
 }
