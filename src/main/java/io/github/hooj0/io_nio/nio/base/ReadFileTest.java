4834e375bb142f0ceaf5ca6908b7873f7ec6c62d
hoojo <hoojo@qq.com>
2021-08-24 16:32:57 +0800

    :sparkles: :bento: :truck:  Renamed 读文件测试

--- src/main/java/io/github/hooj0/io_nio/nio/base/ReadFileTest.java
+++ src/main/java/io/github/hooj0/io_nio/nio/base/ReadFileTest.java
@@ -1,4 +1,6 @@
-package io.github.hooj0.nio.base;
+package io.github.hooj0.io_nio.nio.base;
+
+import io.github.hooj0.BasedTests;
 
 import java.io.FileInputStream;
 import java.nio.ByteBuffer;
@@ -7,10 +9,17 @@ import java.nio.channels.FileChannel;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
 
-public class ReadFileTest {
+/**
+ * 读文件测试
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/01/26 09:25:34
+ */
+@SuppressWarnings("ALL")
+public class ReadFileTest extends BasedTests {
 
 	/**
-	 * <b>function:</b>
 	 * @author hoojo
 	 * @createDate Jan 2, 2011 4:24:31 PM
 	 * @param args
@@ -20,15 +29,19 @@ public class ReadFileTest {
 		try {
 			FileInputStream fis = new FileInputStream("file/info.txt");
 			channel = fis.getChannel();
+
 			//定义一个ByteBuffer对象，用于重复取水
 			ByteBuffer buffer = ByteBuffer.allocate(1024);
+
 			//将FileChannel中数据放入ByteBuffer中
 			while (channel.read(buffer) > 0) {
 				buffer.flip();//锁定到空白区域
+
 				Charset charset = Charset.forName("GBK");
 				CharsetDecoder decoder = charset.newDecoder();
 				CharBuffer cb = decoder.decode(buffer);
-				System.out.println(cb);
+				out(cb);
+
 				buffer.clear();
 			}
 		} catch (Exception e) {
