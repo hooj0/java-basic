f27e27bd16c034bd9619b63914c03a550957f4f4
hoojo <hoojo@qq.com>
2021-08-24 20:34:57 +0800

    :sparkles: :bento: :tada:  Init add <b>function:</b> 情报工厂代理

---
+++ src/main/java/io/github/hooj0/reflection/dynamicproxy/InformactionProxyFactory.java
@@ -0,0 +1,30 @@
+package io.github.hooj0.reflection.dynamicproxy;
+
+import java.lang.reflect.Proxy;
+
+/**
+ * <b>function:</b> 情报工厂代理
+ * @author hoojo
+ * @createDate Oct 6, 2010 7:01:54 PM
+ * @file InformactionProxyFactory.java
+ * @package com.hoo.base.dynamicproxy
+ * @project JavaReflection
+ * @blog http://blog.csdn.net/IBM_hoojo
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class InformactionProxyFactory {
+	/**
+	 * <b>function:</b>传入一个线人和一个劫匪
+	 * @author hoojo
+	 * @createDate Oct 6, 2010 7:39:59 PM
+	 * @param helper
+	 * @param info
+	 * @return GatherInformation
+	 */
+	public static Object getInformaction(SkillHelper helper, GatherInformation info) {
+		Intelligence handler = new Intelligence(helper);
+		handler.setTarget(info);
+		return Proxy.newProxyInstance(info.getClass().getClassLoader(), info.getClass().getInterfaces(), handler);
+	}
+}
