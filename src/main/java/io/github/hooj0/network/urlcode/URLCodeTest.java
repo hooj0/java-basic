dc15f6dc8aa19e3fdcd7870111a1b11dd9986787
hoojo <hoojo@qq.com>
2021-08-26 20:58:56 +0800

    :sparkles: :bento: :recycle:  Changed urlcode 测试

--- src/main/java/io/github/hooj0/network/urlcode/URLCodeTest.java
+++ src/main/java/io/github/hooj0/network/urlcode/URLCodeTest.java
@@ -1,23 +1,27 @@
 package io.github.hooj0.network.urlcode;
 
+import io.github.hooj0.BasedTests;
+
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 
-public class URLCodeTest {
+/**
+ * urlcode 测试
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/02/07 15:08:36
+ */
+public class URLCodeTest extends BasedTests {
 
-	/**
-	 * @param args
-	 * @throws UnsupportedEncodingException 
-	 */
 	public static void main(String[] args) throws UnsupportedEncodingException {
 		//将普通字符串转换成application/x-www-form-urlencoded
 		String keyWord = URLEncoder.encode("中国", "UTF-8");
-		System.out.println(keyWord);
+		out(keyWord);
 		
 		//将application/x-www-form-urlencoded字符串转换成普通字符前
 		String encode = URLDecoder.decode(keyWord, "UTF-8");
-		System.out.println(encode);
+		out(encode);
 	}
-
 }
