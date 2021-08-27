c4264319e469b8e556bfac2682753929d156656c
hoojo <hoojo@qq.com>
2021-08-27 15:59:56 +0800

    :sparkles: :bento: :recycle:  Changed 输入流读取器测试

--- src/main/java/io/github/hooj0/io_nio/io/reader/InputStreamReaderTest.java
+++ src/main/java/io/github/hooj0/io_nio/io/reader/InputStreamReaderTest.java
@@ -4,22 +4,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * 输入流读取器测试
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date Dec 13, 2010 12:28:56 AM
+ */
 public class InputStreamReaderTest {
 
-	/**
-	 *
-
-	 * @createDate Dec 13, 2010 12:28:56 AM
-	 * @param args
-	 * @throws IOException 
-	 */
 	public static void main(String[] args) throws IOException {
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
 		/*
 		 * System.in包装成BufferedReader，bufferedReader具有一个缓冲功能
 		 * 一次读取一行文本，直到读到换行字才输出数据，没有读到换行将阻塞，直到读到换行符为真
-		 * 
 		 */
 		String buffer = null;
 		while ((buffer = br.readLine()) != null) {
