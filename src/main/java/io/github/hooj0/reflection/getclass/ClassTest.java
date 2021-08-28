fa3a760e67b93f6d19e0377217a7ecfaaba73f54
hoojo <hoojo@qq.com>
2021-08-28 18:30:56 +0800

    :sparkles: :bento: :recycle:  Changed 类测试

--- src/main/java/io/github/hooj0/reflection/getclass/ClassTest.java
+++ src/main/java/io/github/hooj0/reflection/getclass/ClassTest.java
@@ -1,33 +1,40 @@
 package io.github.hooj0.reflection.getclass;
 
+import io.github.hooj0.BasedTests;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class ClassTest {
+/**
+ * 类测试
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date Oct 6, 2010 2:43:30 PM
+ */
+public class ClassTest extends BasedTests {
 
-	/**
-	 *
-	 * @author hoojo
-	 * @createDate Oct 6, 2010 2:43:30 PM
-	 * @param args
-	 * @throws Exception 
-	 */
 	public static void main(String[] args) throws Exception {
 		Class<?> clazz1 = Class.forName("java.util.Date");
-		System.out.println(clazz1);
+		out(clazz1);
+
 		Class<?> clazz2 = Date.class;
 		Class<?> c = int[].class;
-		System.out.println(c);
-		System.out.println(clazz2);
+		out(c);
+		out(clazz2);
+
 		String name = "abcd";
 		Class<?> clazz3 = name.getClass();
-		System.out.println(clazz3);
+		out(clazz3);
+
 		Class<?> clazz4 = SimpleDateFormat.class.getSuperclass();
-		System.out.println(clazz4);
+		out(clazz4);
+
 		Class<?> clazz5 = Boolean.TYPE;
 		Class<?> clazz6 = Boolean.class;
-		System.out.println((clazz5 == clazz6) + ":" + clazz5 + "," + clazz6);
+		out((clazz5 == clazz6) + ":" + clazz5 + "," + clazz6);
+
 		Class<?> clazz7 = ClassLoader.getSystemClassLoader().loadClass("java.util.Date");
-		System.out.println(clazz7);
+		out(clazz7);
 	}
 }
