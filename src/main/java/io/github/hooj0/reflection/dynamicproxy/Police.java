ef4625ecf05f608f0b9fbf2be3afeec41eeea5e5
hoojo <hoojo@qq.com>
2021-09-03 10:19:56 +0800

    :sparkles: :bento: :recycle:  Changed 警察

--- src/main/java/io/github/hooj0/reflection/dynamicproxy/Police.java
+++ src/main/java/io/github/hooj0/reflection/dynamicproxy/Police.java
@@ -1,14 +1,23 @@
 package io.github.hooj0.reflection.dynamicproxy;
 
+/**
+ * 警察
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/02/07 11:51:29
+ */
 public class Police implements GatherInformation {
-	private GatherInformation info;
-	public void message(String msg) {
-		info = new XiGui();
-		info.message(msg);
-	}
-	
-	public static void main(String[] args) {
-		Police p = new Police();
-		p.message("police 要情报！");
-	}
+
+    private GatherInformation info;
+
+    public void message(String msg) {
+        info = new XiGui();
+        info.message(msg);
+    }
+
+    public static void main(String[] args) {
+        Police p = new Police();
+        p.message("police 要情报！");
+    }
 }
