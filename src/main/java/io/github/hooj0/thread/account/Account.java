9aa46ec06f67141a61ced49fe3f2e15b683fb41f
hoojo <hoojo@qq.com>
2021-09-08 17:01:57 +0800

    :sparkles: :bento: :tada:  Init add <b>function:</b>取款对象，非线程安全

---
+++ src/main/java/io/github/hooj0/thread/account/Account.java
@@ -0,0 +1,47 @@
+package io.github.hooj0.thread.account;
+
+/**
+ * <b>function:</b>取款对象，非线程安全
+ * @author hoojo
+ * @createDate Nov 2, 2010 10:28:01 PM
+ * @file Account.java
+ * @package com.hoo.thread.account
+ * @project JavaThread
+ * @blog http://blog.csdn.net/IBM_hoojo
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class Account {
+	private String number;
+	private double money;
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public double getMoney() {
+		return money;
+	}
+	public void setMoney(double money) {
+		this.money = money;
+	}
+	public Account(String number, double money) {
+		this.number = number;
+		this.money = money;
+	}
+	
+	@Override
+	public int hashCode() {
+		return this.number.hashCode();
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (o != null && o.getClass() == Account.class) {
+			Account a = (Account) o;
+			return a.getNumber().equals(number);
+		}
+		return false;
+	}
+}
