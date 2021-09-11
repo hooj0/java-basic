fd9e8d0454a51b8a78dbfb028613deb2ff8af1a4
hoojo <hoojo@qq.com>
2021-09-11 09:48:57 +0800

    :sparkles: :bento: :tada:  Init add 	<b>function:</b>有序列表

---
+++ src/main/java/io/github/hooj0/collection/set/LinkHashSetTest.java
@@ -0,0 +1,23 @@
+package io.github.hooj0.collection.set;
+
+import java.util.LinkedHashSet;
+
+public class LinkHashSetTest {
+
+	/**
+	 * <b>function:</b>有序列表
+	 * @author hoojo
+	 * @createDate Jan 9, 2011 4:41:14 PM
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		LinkedHashSet<String> books = new LinkedHashSet<String>();
+		books.add("Java");
+		books.add("Struts");
+		System.out.println(books);
+		books.remove("Struts");
+		System.out.println(books);
+		books.add("Struts");
+		System.out.println(books);
+	}
+}
