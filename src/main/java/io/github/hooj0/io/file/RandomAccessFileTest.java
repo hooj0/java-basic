274bab2622e9723b2c759c10b3b3c30f13cc085f
hoojo <hoojo@qq.com>
2021-09-14 12:02:57 +0800

    :sparkles: :bento: :tada:  Init add 	<b>function:</b>

---
+++ src/main/java/io/github/hooj0/io/file/RandomAccessFileTest.java
@@ -0,0 +1,32 @@
+package io.github.hooj0.io.file;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class RandomAccessFileTest {
+
+	/**
+	 * <b>function:</b>
+
+	 * @createDate Dec 13, 2010 1:53:48 PM
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		String path = System.getProperty("user.dir");
+		//以只读的方式打开指定文件
+		RandomAccessFile raf = new RandomAccessFile(path + "/src/com/hoo/io/file/RandomAccessFileTest.java", "r");
+		//获取raf的初始指针位置
+		System.out.println("初始指针位置：" + raf.getFilePointer());
+		//移动raf的文件记录指针位置
+		raf.seek(200);
+		byte[] buff = new byte[1024];
+		//用于保存实际读取字节数
+		int len = 0;
+		while ((len = raf.read(buff)) > 0) {
+			System.out.println(new String(buff, 0, len));
+			System.out.println(new String(buff, "utf-8"));
+		}
+		raf.close();
+	}
+}
