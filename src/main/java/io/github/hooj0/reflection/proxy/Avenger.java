6a4356ea873fbe5f4a51b0dd514d50bbca209be5
hoojo <hoojo@qq.com>
2021-09-19 09:47:57 +0800

    :sparkles: :bento: :tada:  Init add 	<b>function:</b>

---
+++ src/main/java/io/github/hooj0/reflection/proxy/Avenger.java
@@ -0,0 +1,16 @@
+package io.github.hooj0.reflection.proxy;
+
+public class Avenger {
+
+	/**
+	 * <b>function:</b>
+	 * @author hoojo
+	 * @createDate Oct 6, 2010 5:44:15 PM
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//复仇者想要杀掉伊拉克，但他又不能亲手杀人。只好委托中间人帮他杀掉伊拉克
+		KillPerson kill = new Client();
+		kill.killPerson("伊拉克");
+	}
+}
