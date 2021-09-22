77208dc77c5044201d47a95d10fb92909712695e
hoojo <hoojo@qq.com>
2021-09-22 15:10:57 +0800

    :sparkles: :bento: :tada:  Init add 	<b>function:</b>

---
+++ src/main/java/io/github/hooj0/exception/FAQTryCatchTest.java
@@ -0,0 +1,27 @@
+package io.github.hooj0.exception;
+
+public class FAQTryCatchTest {
+
+	public int method() {
+		try {
+			return 1/0;
+		} catch (Exception e) {
+			System.out.println("catch");
+			return 1;
+		} finally {
+			System.out.println("finally");
+			return 3;
+		}
+	}
+	
+	/**
+	 * <b>function:</b>
+	 * @author hoojo
+	 * @createDate Mar 3, 2011 10:50:59 PM
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(new FAQTryCatchTest().method());
+	}
+
+}
