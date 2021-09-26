0dc4a5858eba1470603aa7b107bf6d08ae1156dd
hoojo <hoojo@qq.com>
2021-09-26 16:17:57 +0800

    :sparkles: :bento: :tada:  Init add  constom protocol java

---
+++ src/main/java/io/github/hooj0/network/udp/multicast/chart/ConstomProtocol.java
@@ -0,0 +1,9 @@
+package io.github.hooj0.network.udp.multicast.chart;
+
+public final class ConstomProtocol {
+	//定义协议字符串的长度
+	public static int PROTOCOL_LENGHT = 2;
+	public static String PRESENCE = "★【";
+	public static String SPLITTER = "※";
+	
+}
