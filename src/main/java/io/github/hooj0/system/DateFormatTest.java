9c24f82b9efde08a585a11e718de03c91b4994bd
hoojo <hoojo@qq.com>
2021-10-02 21:47:56 +0800

    :sparkles: :bento: :recycle:  Changed 日期格式测试

--- src/main/java/io/github/hooj0/system/DateFormatTest.java
+++ src/main/java/io/github/hooj0/system/DateFormatTest.java
@@ -1,21 +1,25 @@
 package io.github.hooj0.system;
 
+import io.github.hooj0.BasedTests;
+
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-public class DateFormatTest {
+/**
+ * 日期格式测试
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date Feb 25, 2011 11:18:56 PM
+ */
+public class DateFormatTest extends BasedTests {
 
-	/**
-	 * 
-	 * @author hoojo
-	 * @createDate Feb 25, 2011 11:18:56 PM
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		Date d = new Date();
 		Locale[] locales = { Locale.CHINA, Locale.UK };
 		DateFormat[] df = new DateFormat[16];
+
 		for (int i = 0; i < locales.length; i++) {
 			df[i * 8] = DateFormat.getDateInstance(DateFormat.SHORT, locales[i]);
 			df[i * 8 + 1] = DateFormat.getDateInstance(DateFormat.MEDIUM, locales[i]);
@@ -26,23 +30,27 @@ public class DateFormatTest {
 			df[i * 8 + 6] = DateFormat.getTimeInstance(DateFormat.LONG, locales[i]);
 			df[i * 8 + 7] = DateFormat.getTimeInstance(DateFormat.FULL, locales[i]);
 		}
+
 		for (int i = 0; i < locales.length; i++) {
 			switch (i) {
 				case 0:
-					System.out.println("------中国-------");
+					out("------中国-------");
 					break;
 				case 1:
-					System.out.println("------美国-------");
+					out("------美国-------");
 					break;
+				default:
+					continue;
 			}
-			System.out.println("short格式日期：" + df[i * 8].format(d));
-			System.out.println("medium格式日期：" + df[i * 8 + 1].format(d));
-			System.out.println("long格式日期：" + df[i * 8 + 2].format(d));
-			System.out.println("full格式日期：" + df[i * 8 + 3].format(d));
-			System.out.println("short格式日期：" + df[i * 8 + 4].format(d));
-			System.out.println("medium格式日期：" + df[i * 8 + 5].format(d));
-			System.out.println("long格式日期：" + df[i * 8 + 6].format(d));
-			System.out.println("full格式日期：" + df[i * 8 + 7].format(d));
+
+			out("short格式日期：" + df[i * 8].format(d));
+			out("medium格式日期：" + df[i * 8 + 1].format(d));
+			out("long格式日期：" + df[i * 8 + 2].format(d));
+			out("full格式日期：" + df[i * 8 + 3].format(d));
+			out("short格式日期：" + df[i * 8 + 4].format(d));
+			out("medium格式日期：" + df[i * 8 + 5].format(d));
+			out("long格式日期：" + df[i * 8 + 6].format(d));
+			out("full格式日期：" + df[i * 8 + 7].format(d));
 		}
 	}
 }
