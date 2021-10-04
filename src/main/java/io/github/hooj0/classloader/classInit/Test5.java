f1b098ed3e7ab2b41c39e76d9ae2b6fc8c4a814b
hoojo <hoojo@qq.com>
2021-10-04 09:09:57 +0800

    :sparkles: :bento: :tada:  Init add 		由于timed的值只能在运行的时候得到，编译的时候无法获取值

---
+++ src/main/java/io/github/hooj0/classloader/classInit/Test5.java
@@ -0,0 +1,18 @@
+package io.github.hooj0.classloader.classInit;
+
+public class Test5 {
+	public static void main(String[] args) {
+		System.out.println(Temp.timed);
+		/**
+		 * 由于timed的值只能在运行的时候得到，编译的时候无法获取值
+		 * 所以通过类访问静态属性的时候，会导致该类初始化。从而static运行了
+		 */
+	}
+}
+
+class Temp {
+	static {
+		System.out.println("运行……");
+	}
+	static long timed = System.currentTimeMillis();
+}
