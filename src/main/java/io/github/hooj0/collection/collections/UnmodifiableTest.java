934a475fea47ba45baf708ab68c7cb65eb629745
hoojo <hoojo@qq.com>
2021-10-04 18:26:57 +0800

    :sparkles: :bento: :tada:  Init add  unmodifiable test java

---
+++ src/main/java/io/github/hooj0/collection/collections/UnmodifiableTest.java
@@ -0,0 +1,30 @@
+package io.github.hooj0.collection.collections;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class UnmodifiableTest {
+
+	public static void main(String[] args) {
+		//创建一个空的不可变的List对象
+		List<String> emptyList = Collections.emptyList();
+		
+		//创建一个只有一个元素、且不可变的Set对象
+		Set<String> unmodifiableSet = Collections.singleton("Java");
+		
+		//创建一个普通Map
+		Map<String, Integer> scores = new HashMap<String, Integer>();
+		scores.put("语文", 60);
+		scores.put("Java", 90);
+		
+		Map<String, Integer> unmodifiableMap = Collections.unmodifiableMap(scores);
+		//测试数据是否可以添加，java.lang.UnsupportedOperationException不可变的
+		emptyList.add("test");
+		unmodifiableSet.add("DB");
+		unmodifiableMap.put("高数", 88);
+		
+	}
+}
