5dc14357cc22f7df3b4ffce73f6f2992324fec85
hoojo <hoojo@qq.com>
2021-10-05 20:34:56 +0800

    :sparkles: :bento: :recycle:  Changed 系统信息

--- src/main/java/io/github/hooj0/io_nio/io/base/SystemInfo.java
+++ src/main/java/io/github/hooj0/io_nio/io/base/SystemInfo.java
@@ -1,52 +1,54 @@
 package io.github.hooj0.io_nio.io.base;
 
-import java.util.Collection;
-import java.util.Enumeration;
-import java.util.Iterator;
-import java.util.Properties;
-import java.util.Set;
+import io.github.hooj0.BasedTests;
+
+import java.util.*;
 import java.util.Map.Entry;
 
-public class SystemInfo {
+/**
+ * 系统信息
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date Dec 12, 2010 4:34:36 PM
+ */
+public class SystemInfo extends BasedTests {
 
-	/**
-	 * 
+    public static void main(String[] args) {
+        Properties pro = System.getProperties();
+        Set<Entry<Object, Object>> s = pro.entrySet();
+        Iterator<Entry<Object, Object>> iter = s.iterator();
+        while (iter.hasNext()) {
+            Entry<Object, Object> ety = iter.next();
+            out(ety.getKey() + "##" + ety.getValue());
+        }
 
-	 * @createDate Dec 12, 2010 4:34:36 PM
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		Properties pro = System.getProperties();
-		Set<Entry<Object, Object>> s = pro.entrySet();
-		Iterator<Entry<Object, Object>> iter = s.iterator();
-		while(iter.hasNext()) {
-			Entry<Object, Object> ety = iter.next();
-			System.out.println(ety.getKey() + "##" + ety.getValue());
-		}
-		System.out.println("##########################");
-		Set<Object> set = pro.keySet();
-		for (Object o : set) {
-			System.out.println(o + "=" + pro.getProperty(o.toString()) + "###" + pro.get(o));
-		}
-		System.out.println("##########################");
-		Enumeration<Object> en = pro.keys();
-		while(en.hasMoreElements()) {
-			Object o = en.nextElement();
-			System.out.println(o + "%%%" + pro.getProperty(o.toString()) + "###" + pro.get(o));
-		}
-		System.out.println("##########################");
-		Enumeration<Object> ele = pro.elements();
-		while(ele.hasMoreElements()) {
-			Object o = ele.nextElement();
-			System.out.println(o);
-		}
-		
-		System.out.println("————————————————————————————————————");
-		Collection<Object> v = pro.values();
-		Iterator<Object> o = v.iterator();
-		while(o.hasNext()) {
-			Object value = o.next();
-			System.out.println(value);
-		}
-	}
+        out("##########################");
+        Set<Object> set = pro.keySet();
+        for (Object o : set) {
+            out(o + "=" + pro.getProperty(o.toString()) + "###" + pro.get(o));
+        }
+
+        out("##########################");
+        Enumeration<Object> en = pro.keys();
+        while (en.hasMoreElements()) {
+            Object o = en.nextElement();
+            out(o + "%%%" + pro.getProperty(o.toString()) + "###" + pro.get(o));
+        }
+
+        out("##########################");
+        Enumeration<Object> ele = pro.elements();
+        while (ele.hasMoreElements()) {
+            Object o = ele.nextElement();
+            out(o);
+        }
+
+        out("————————————————————————————————————");
+        Collection<Object> v = pro.values();
+        Iterator<Object> o = v.iterator();
+        while (o.hasNext()) {
+            Object value = o.next();
+            out(value);
+        }
+    }
 }
