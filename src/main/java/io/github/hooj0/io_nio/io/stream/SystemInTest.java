70368b564ee1be461ffce66775e10db04bb73b65
hoojo <hoojo@qq.com>
2021-10-08 18:31:56 +0800

    :sparkles: :bento: :recycle:  Changed 系统测试

--- src/main/java/io/github/hooj0/io_nio/io/stream/SystemInTest.java
+++ src/main/java/io/github/hooj0/io_nio/io/stream/SystemInTest.java
@@ -4,15 +4,15 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Scanner;
 
+/**
+ * 系统测试
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date Dec 13, 2010 12:34:22 PM
+ */
 public class SystemInTest {
 
-	/**
-	 * 
-
-	 * @createDate Dec 13, 2010 12:34:22 PM
-	 * @param args
-	 * @throws IOException 
-	 */
 	public static void main(String[] args) throws IOException {
 		String path = System.getProperty("user.dir");
 		FileInputStream fis = new FileInputStream(path + "/file/temp.txt");
