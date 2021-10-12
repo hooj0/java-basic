9679b317682883c258d24f691933b4b4411eb1f9
hoojo <hoojo@qq.com>
2021-10-12 16:17:56 +0800

    :sparkles: :bento: :recycle:  Changed url test

--- src/main/java/io/github/hooj0/network/url/URLTest.java
+++ src/main/java/io/github/hooj0/network/url/URLTest.java
@@ -1,27 +1,31 @@
 package io.github.hooj0.network.url;
 
+import io.github.hooj0.BasedTests;
+
 import java.net.URL;
 
-public class URLTest {
+/**
+ * url test
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/02/07 15:08:06
+ */
+public class URLTest extends BasedTests {
 
-	/**
-	 * @param args
-	 * @throws Exception 
-	 */
-	public static void main(String[] args) throws Exception {
-		URL url = new URL("http://localhost:8080/API/ext2/docs/index.html?query=背影&author=朱自清");
-		System.out.println("访问资源名称：" + url.getFile());
-		System.out.println("主机名称：" + url.getHost());
-		System.out.println("获取访问的路径：" + url.getPath());
-		System.out.println("获取访问的端口：" + url.getPort());
-		System.out.println("获取访问的默认端口：" + url.getDefaultPort());
-		System.out.println("获取协议名称：" + url.getProtocol());
-		System.out.println("获取查询字符串部分：" + url.getQuery());//?号后面的参数部分
-		System.out.println("授权：" + url.getAuthority());
-		System.out.println("内容：" + url.getContent());
-		System.out.println("获取访问资源引用：" + url.getRef());
-		System.out.println("获取userInfo：" + url.getUserInfo());
-		System.out.println(url.toURI().getQuery());
-	}
-
+    public static void main(String[] args) throws Exception {
+        URL url = new URL("http://localhost:8080/API/ext2/docs/index.html?query=背影&author=朱自清");
+        out("访问资源名称：" + url.getFile());
+        out("主机名称：" + url.getHost());
+        out("获取访问的路径：" + url.getPath());
+        out("获取访问的端口：" + url.getPort());
+        out("获取访问的默认端口：" + url.getDefaultPort());
+        out("获取协议名称：" + url.getProtocol());
+        out("获取查询字符串部分：" + url.getQuery());//?号后面的参数部分
+        out("授权：" + url.getAuthority());
+        out("内容：" + url.getContent());
+        out("获取访问资源引用：" + url.getRef());
+        out("获取userInfo：" + url.getUserInfo());
+        out(url.toURI().getQuery());
+    }
 }
