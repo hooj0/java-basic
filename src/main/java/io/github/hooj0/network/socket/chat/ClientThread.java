6c280d5e1012b622d985693d069e4f00b964ca59
hoojo <hoojo@qq.com>
2021-10-15 16:24:56 +0800

    :sparkles: :bento: :recycle:  Changed Socket通讯客户端多线程代码

--- src/main/java/io/github/hooj0/network/socket/chat/ClientThread.java
+++ src/main/java/io/github/hooj0/network/socket/chat/ClientThread.java
@@ -7,34 +7,37 @@ import java.net.Socket;
 
 /**
  * Socket通讯客户端多线程代码
+ *
  * @author hoojo
+ * @version 1.0
  * @createDate Sep 23, 2010 10:54:59 AM
  * @file ClientThread.java
  * @package com.hoo.base.socket.chat
  * @project JavaNetBase
  * @blog http://blog.csdn.net/IBM_hoojo
  * @email hoojo_@126.com
- * @version 1.0
  */
 public class ClientThread implements Runnable {
-	//Socket连接线程
-	//private Socket s = null;
-	//处理Socket的输入流
-	private BufferedReader br = null;
-	
-	public ClientThread(Socket s) throws IOException {
-		//this.s = s;
-		br = new BufferedReader(new InputStreamReader(s.getInputStream()));
-	}
 
-	public void run() {
-		try {
-			String content = "";
-			while ((content = br.readLine()) != null) {
-				System.out.println(content);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    //Socket连接线程
+    //private Socket s = null;
+    //处理Socket的输入流
+    private BufferedReader br = null;
+
+    public ClientThread(Socket s) throws IOException {
+        //this.s = s;
+	    this.br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+    }
+
+    @Override
+    public void run() {
+        try {
+            String content = "";
+            while ((content = this.br.readLine()) != null) {
+                System.out.println(content);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
