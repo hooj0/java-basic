f0f025746169ddd92cbe202b062ead9fb286e188
hoojo <hoojo@qq.com>
2021-10-16 14:25:56 +0800

    :sparkles: :bento: :truck:  Renamed const协议

--- src/main/java/io/github/hooj0/network/udp/multicast/chat/ConstProtocol.java
+++ src/main/java/io/github/hooj0/network/udp/multicast/chat/ConstProtocol.java
@@ -1,6 +1,13 @@
 package io.github.hooj0.network.udp.multicast.chat;
 
-public final class ConstomProtocol {
+/**
+ * const协议
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date 2022/02/07 15:06:11
+ */
+public final class ConstProtocol {
 	//定义协议字符串的长度
 	public static int PROTOCOL_LENGHT = 2;
 	public static String PRESENCE = "★【";
