3e6cffd5a6a9c0763f4d4ffe7de20255534fc58b
hoojo <hoojo@qq.com>
2021-10-19 09:46:57 +0800

    :sparkles: :bento: :tada:  Init add 	<b>function:</b>

---
+++ src/main/java/io/github/hooj0/collection/collections/SortTest.java
@@ -0,0 +1,35 @@
+package io.github.hooj0.collection.collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SortTest {
+
+	/**
+	 * <b>function:</b>
+	 * @author hoojo
+	 * @createDate Jan 14, 2011 8:43:16 PM
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ArrayList<Integer> nums = new ArrayList<Integer>();
+		nums.add(2);
+		nums.add(-5);
+		nums.add(3);
+		nums.add(0);
+		
+		System.out.println(nums);
+		
+		//排序
+		Collections.sort(nums);
+		System.out.println(nums);
+		
+		//翻转次序
+		Collections.reverse(nums);
+		System.out.println(nums);
+		
+		//随机排序
+		Collections.shuffle(nums);
+		System.out.println(nums);
+	}
+}
