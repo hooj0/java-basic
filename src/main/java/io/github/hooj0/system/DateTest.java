c32b8acd99ffd3ca96e21d897b7c2c0ebe525fe8
hoojo <hoojo@qq.com>
2021-10-20 16:54:56 +0800

    :sparkles: :bento: :recycle:  Changed 测试日期

--- src/main/java/io/github/hooj0/system/DateTest.java
+++ src/main/java/io/github/hooj0/system/DateTest.java
@@ -1,21 +1,24 @@
 package io.github.hooj0.system;
 
+import io.github.hooj0.BasedTests;
+
 import java.util.Date;
 
-public class DateTest {
+/**
+ * 测试日期
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date Feb 13, 2011 11:05:06 PM
+ */
+public class DateTest extends BasedTests {
 
-	/**
-	 *
-	 * @author hoojo
-	 * @createDate Feb 13, 2011 11:05:06 PM
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		Date d1 = new Date();
 		//获取当前时间之后的100ms的时间
 		Date d2 = new Date(System.currentTimeMillis() + 100);
-		System.out.println(d2);
-		System.out.println(d1.compareTo(d2));
-		System.out.println(d1.before(d2));
+		out(d2);
+		out(d1.compareTo(d2));
+		out(d1.before(d2));
 	}
 }
