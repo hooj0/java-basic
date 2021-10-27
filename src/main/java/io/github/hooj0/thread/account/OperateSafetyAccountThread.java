68a9b2e60bd700a243dbceafc6c1fe3c6027ee64
hoojo <hoojo@qq.com>
2021-10-27 17:53:56 +0800

    :sparkles: :bento: :recycle:  Changed 取款操作线程类

--- src/main/java/io/github/hooj0/thread/account/OperateSafetyAccountThread.java
+++ src/main/java/io/github/hooj0/thread/account/OperateSafetyAccountThread.java
@@ -2,28 +2,32 @@ package io.github.hooj0.thread.account;
 
 /**
  * 取款操作线程类
+ *
  * @author hoojo
+ * @version 1.0
  * @createDate Nov 2, 2010 10:28:25 PM
  * @file OperateAccountThread.java
  * @package com.hoo.thread.account
  * @project JavaThread
  * @blog http://blog.csdn.net/IBM_hoojo
  * @email hoojo_@126.com
- * @version 1.0
  */
 public class OperateSafetyAccountThread extends Thread {
-	private SafetyAccount acc;
-	private double drawMoney;
-	public OperateSafetyAccountThread(String name, SafetyAccount acc, double drawMoney) {
-		this.setName(name);
-		this.acc = acc;
-		this.drawMoney = drawMoney;
-	}
-	
-	public void run() {
-		/*
-		 * 线程安全方法
-		 */
-		acc.drawMoneyMethod(drawMoney);
-	}
+
+    private final SafetyAccount account;
+    private final double drawMoney;
+
+    public OperateSafetyAccountThread(String name, SafetyAccount account, double drawMoney) {
+        this.setName(name);
+        this.account = account;
+        this.drawMoney = drawMoney;
+    }
+
+    @Override
+    public void run() {
+        /*
+         * 线程安全方法
+         */
+	    this.account.drawMoneyMethod(this.drawMoney);
+    }
 }
