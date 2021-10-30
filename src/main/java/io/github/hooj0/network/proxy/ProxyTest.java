052eca263a90b8e6290f4d685c9cd6d07779f215
hoojo <hoojo@qq.com>
2021-10-30 14:10:56 +0800

    :sparkles: :bento: :recycle:  Changed 网络代理链接

--- src/main/java/io/github/hooj0/network/proxy/ProxyTest.java
+++ src/main/java/io/github/hooj0/network/proxy/ProxyTest.java
@@ -1,5 +1,7 @@
 package io.github.hooj0.network.proxy;
 
+import io.github.hooj0.BasedTests;
+
 import java.io.PrintStream;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
@@ -8,64 +10,59 @@ import java.net.URLConnection;
 import java.util.Scanner;
 
 /**
- *  网络代理链接
+ * 网络代理链接
+ *
  * @author hoojo
+ * @version 1.0
  * @createDate Sep 24, 2010 3:04:05 PM
  * @file ProxyTest.java
  * @package com.hoo.base.proxy
  * @project JavaNetBase
  * @blog http://blog.csdn.net/IBM_hoojo
  * @email hoojo_@126.com
- * @version 1.0
  */
-public class ProxyTest {
-	private Proxy proxy;
-	private URL url;
-	private URLConnection conn;
-	//通过代理读取网络数据
-	private Scanner scan;
-	private PrintStream ps;
-	//设置ip和端口
-	private String address = "127.0.0.1";
-	private int port = 30000;
-	
-	//要请求的网址
-	String uri = "http://localhost:8080/JavaNetBase/index.jsp";
-	
-	public void init() {
-		try {
-			url = new URL(uri);
-			//创建代理服务器
-			proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(address, port));
-			//使用代理打开连接
-			conn = url.openConnection(proxy);
-			//设置超时长
-			conn.setConnectTimeout(5000);
-			scan = new Scanner(conn.getInputStream());
-			//初始化输出流
-			ps = new PrintStream("index.jsp");
-			while (scan.hasNext()) {
-				String line = scan.nextLine();
-				System.out.println(line);
-				ps.println(line);
-			}
-		} catch (Exception e) {
-			System.out.println(uri + "无法访问！");
-		} finally {
-			if (ps != null) {
-				ps.close();
-			}
-		}
-	}
-	
-	/**
-	 * 
-	 * @author hoojo
-	 * @createDate Sep 24, 2010 3:04:01 PM
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		new ProxyTest().init();
-	}
+public class ProxyTest extends BasedTests {
 
+    private Proxy proxy;
+    private URL url;
+    private URLConnection conn;
+    //通过代理读取网络数据
+    private Scanner scan;
+    private PrintStream ps;
+    //设置ip和端口
+    private final String address = "127.0.0.1";
+    private final int port = 30000;
+
+    //要请求的网址
+    String uri = "http://localhost:8080/JavaNetBase/index.jsp";
+
+    public void init() {
+        try {
+	        this.url = new URL(this.uri);
+            //创建代理服务器
+	        this.proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(this.address, this.port));
+            //使用代理打开连接
+	        this.conn = this.url.openConnection(this.proxy);
+            //设置超时长
+	        this.conn.setConnectTimeout(5000);
+	        this.scan = new Scanner(this.conn.getInputStream());
+            //初始化输出流
+	        this.ps = new PrintStream("index.jsp");
+            while (this.scan.hasNext()) {
+                String line = this.scan.nextLine();
+                out(line);
+	            this.ps.println(line);
+            }
+        } catch (Exception e) {
+            out(this.uri + "无法访问！");
+        } finally {
+            if (this.ps != null) {
+	            this.ps.close();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new ProxyTest().init();
+    }
 }
