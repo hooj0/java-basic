49930d1db2dcac4aee47761c6eb4f9931a2cbbc2
hoojo <hoojo@qq.com>
2021-11-20 11:25:56 +0800

    :sparkles: :bento: :recycle:  Changed 读对象

--- src/main/java/io/github/hooj0/io_nio/io/serializable/ReadObject.java
+++ src/main/java/io/github/hooj0/io_nio/io/serializable/ReadObject.java
@@ -5,23 +5,21 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 
+/**
+ * 读对象
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date Dec 13, 2010 2:58:51 PM
+ */
 public class ReadObject {
 
-	/**
-	 *
-
-	 * @createDate Dec 13, 2010 2:58:51 PM
-	 * @param args
-	 * @throws IOException 
-	 * @throws FileNotFoundException 
-	 * @throws ClassNotFoundException 
-	 */
-	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
-		String path = System.getProperty("user.dir");
-		ObjectInputStream ois = new ObjectInputStream(new FileInputStream(path + "/file/object.txt"));
-		User user = (User) ois.readObject();//反序列化
-		System.out.println(user.getName());
-		System.out.println(user.getAge());
-		ois.close();
-	}
+    public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
+        String path = System.getProperty("user.dir");
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(path + "/file/object.txt"));
+        User user = (User) ois.readObject();//反序列化
+        System.out.println(user.getName());
+        System.out.println(user.getAge());
+        ois.close();
+    }
 }
