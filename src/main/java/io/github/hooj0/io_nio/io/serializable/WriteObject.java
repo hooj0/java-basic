97b8730d7d542f46a76020793410ecc5fc7f0c98
hoojo <hoojo@qq.com>
2021-12-11 21:18:56 +0800

    :sparkles: :bento: :recycle:  Changed 写对象

--- src/main/java/io/github/hooj0/io_nio/io/serializable/WriteObject.java
+++ src/main/java/io/github/hooj0/io_nio/io/serializable/WriteObject.java
@@ -1,55 +1,55 @@
 package io.github.hooj0.io_nio.io.serializable;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.io.*;
 
+/**
+ * 写对象
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date Dec 13, 2010 2:51:33 PM
+ */
 public class WriteObject {
 
-	/**
-	 *
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        String path = System.getProperty("user.dir");
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(path + "/file/object.txt"));
+        User user = new User("tom", 33);
+        oos.writeObject(user);//序列化java对象
+        oos.close();
+    }
 
-	 * @createDate Dec 13, 2010 2:51:33 PM
-	 * @param args
-	 * @throws IOException 
-	 * @throws FileNotFoundException 
-	 */
-	public static void main(String[] args) throws FileNotFoundException, IOException {
-		String path = System.getProperty("user.dir");
-		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(path + "/file/object.txt"));
-		User user = new User("tom", 33);
-		oos.writeObject(user);//序列化java对象
-		oos.close();
-	}
-	
-	/**
-	 * 序列化java对象，在远程调用、分布式开发中常用的方法
-	 * 如EJB、RMI
-	 */
+    /**
+     * 序列化java对象，在远程调用、分布式开发中常用的方法
+     * 如EJB、RMI
+     */
 }
 
 class User implements Serializable {
-	
-	private static final long serialVersionUID = 1L;
-	private String name;
-	private int age;
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
-		this.age = age;
-	}
-	public User(String name, int age) {
-		super();
-		this.name = name;
-		this.age = age;
-	}
+
+    private static final long serialVersionUID = 1L;
+    private String name;
+    private int age;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public User(String name, int age) {
+        super();
+        this.name = name;
+        this.age = age;
+    }
 }
