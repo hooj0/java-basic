9bf5268f1520707f0d91c61a66c0dc9724c1961f
hoojo <hoojo@qq.com>
2021-12-23 18:52:56 +0800

    :sparkles: :bento: :recycle:  Changed 读取用户信息

--- src/main/java/io/github/hooj0/io_nio/io/serializable/ReadUserInfo.java
+++ src/main/java/io/github/hooj0/io_nio/io/serializable/ReadUserInfo.java
@@ -1,21 +1,22 @@
 package io.github.hooj0.io_nio.io.serializable;
 
+import io.github.hooj0.BasedTests;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 
-public class ReadUserInfo {
+/**
+ * 读取用户信息
+ *
+ * @author hoojo
+ * @version 1.0
+ * @date Dec 13, 2010 3:29:15 PM
+ * 	 * @param args
+ */
+public class ReadUserInfo extends BasedTests {
 
-	/**
-	 *
-
-	 * @createDate Dec 13, 2010 3:29:15 PM
-	 * @param args
-	 * @throws IOException 
-	 * @throws FileNotFoundException 
-	 * @throws ClassNotFoundException 
-	 */
 	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
 		String path = System.getProperty("user.dir");
 		ObjectInputStream ois = new ObjectInputStream(new FileInputStream(path + "/file/info.txt"));
@@ -23,10 +24,10 @@ public class ReadUserInfo {
 		UserInfo info2 = (UserInfo) ois.readObject();
 		User user = (User) ois.readObject();
 		UserInfo info3 = (UserInfo) ois.readObject();
-		System.out.println(info.getUser().equals(user));
-		System.out.println(info2.getUser().equals(user));
-		System.out.println(info2 == info3);
-		System.out.println(info.getId() + "#" + info.getUser().getName());
+		out(info.getUser().equals(user));
+		out(info2.getUser().equals(user));
+		out(info2 == info3);
+		out(info.getId() + "#" + info.getUser().getName());
 		ois.close();
 	}
 }
